0598b3ab4f4d38534ebcfc2e6e7eebffb125b7b2
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-05-14 14:27:25 +0000

    temporary commit (issue 99)

---
+++ embox/third-party/emlinux/tools/build_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+FTP_LINK="ftp://ftp.kernel.org/pub/linux/kernel/v2.6/$KERNEL_VERSION"
+export ftp_proxy="http://proxy.tepkom.spb.su:3128"
+
+[ -e $KERNEL_SRC ] || wget -c --proxy=on $FTP_LINK
+[ -d $KERNEL_DIR ] || tar -xvjf $KERNEL_SRC
+
+for p in $KERNEL_PATCHES; do
+    if [[ -e $PATCH_DIR/$p ]]; then
+    	zcat $PATCH_DIR/$p | patch -p0
+    fi
+done
+
+$TOOLS_DIR/romfs-inst.sh
+
+pushd $KERNEL_DIR > /dev/null
+
+cp $PATCH_DIR/$KERNEL_CONFIG config.def
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE menuconfig
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE KBUILD_VERBOSE=1
+
+popd > /dev/null
+
+[ -d $IMAGE_DIR ] || mkdir -p $IMAGE_DIR
+cp $KERNEL_DIR/arch/$ARCH/boot/image $IMAGE_DIR
---
+++ embox/third-party/emlinux/tools/romfs-inst.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+ROMFSIMGOBJ="$IMAGE_DIR/rdimage.o"
+
+[ -d $ROMFS_DIR ] || mkdir -p $ROMFS_DIR
+[ -d $IMAGE_DIR ] || mkdir -p $IMAGE_DIR
+
+for i in `cat $TOOLS_DIR/dirs.lst`; do
+    [ -d $ROMFS_DIR/$i ] || mkdir -p $ROMFS_DIR/$i
+done
+
+#TODO: copy binaries into romfs dir
+
+chmod +x $KERNEL_DIR/scripts/gen_initramfs_list.sh
+$KERNEL_DIR/scripts/gen_initramfs_list.sh $ROMFS_DIR > $KERNEL_DIR/initramfs.lst 2>&1
+
+for i in `cat $TOOLS_DIR/devices.lst`; do
+    name=${i%%,*}
+    i=${i##${name},}
+    type=${i%%,*}
+    i=${i##${type},}
+    maj=${i%%,*}
+    i=${i##${maj},}
+    min=${i%%,*}
+    echo "nod /dev/$name 0600 0 0 $type $maj $min" >> $KERNEL_DIR/initramfs.lst
+done
+
+$GENROMFS -V "ROMdisk" -f $ROMFSIMGOBJ.img -d $ROMFS_DIR
+gzip -f $ROMFSIMGOBJ.img
+${CROSS_COMPILE}ld -r -b binary $ROMFSIMGOBJ.img.gz -o $ROMFSIMGOBJ
+rm -f $ROMFSIMGOBJ.img.gz
