33b21ae23332edcb47cd85b3a501db22b47d5d6b
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-07-06 09:08:59 +0000

    small fix

--- trunk/emlinux/tools/romfs-inst.sh
+++ trunk/emlinux/tools/romfs-inst.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-for i in `cat $TOOLS_DIR/dirs.lst`; do
-    [ -d $ROMFS_DIR/$i ] || mkdir -p $ROMFS_DIR/$i
-done
-
-ln -fs "/sbin/init" $ROMFS_DIR/init
-chmod +x $KERNEL_DIR/scripts/gen_initramfs_list.sh
-$KERNEL_DIR/scripts/gen_initramfs_list.sh $ROMFS_DIR > $KERNEL_DIR/initramfs.lst 2>&1
-
 for i in `cat $TOOLS_DIR/devices.lst`; do
     name=${i%%,*}
     i=${i##${name},}
