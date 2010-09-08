c61f56b76ed27b17e0238ea9a57530c2d69f4cfa
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-09-08 20:04:25 +0000

    2.6.33-leon3 release

--- emlinux/config.mk
+++ emlinux/config.mk
@@ -1,1 +1,8 @@
-ARCH=sparc
+ARCH           = sparc
+KERNEL_VERSION = 2.6.33
+KERNEL_SRC     = linux-$(KERNEL_VERSION).tar.bz2
+KERNEL_PATCHES = linux-$(KERNEL_VERSION)-$(ARCH).patch.gz
+KERNEL_PATCHES+= linux-$(KERNEL_VERSION)-ovk.patch.gz
+KERNEL_CONFIG  = config-$(KERNEL_VERSION)-ovk.default
+KERNEL_URL     = http://www.kernel.org/pub/linux/kernel/v2.6/$(KERNEL_SRC)
+
