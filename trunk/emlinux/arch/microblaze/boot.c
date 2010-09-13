4244a4c56ef25ec7145cf52c2bb9ef53ca134ba0
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-09-13 20:51:52 +0000

    some fix

--- trunk/emlinux/arch/microblaze/boot.c
+++ trunk/emlinux/arch/microblaze/boot.c
@@ -8,8 +8,8 @@
 #include <linux/kernel.h>
 
 //#define XILINX_UARTLITE_BASEADDR  0x84000000
-#define XILINX_UARTLITE_TX_DATA   0x84000020
-#define XILINX_UARTLITE_STATUS    0x84000040
+#define XILINX_UARTLITE_TX_DATA   0x84000004
+#define XILINX_UARTLITE_STATUS    0x84000008
 //#define XILINX_UARTLITE_BAUDRATE  115200
 //#define XILINX_UARTLITE_IRQ_NUM   2
 #define STATUS_TX_FIFO_FULL      (1<<(31-28))
@@ -31,14 +31,32 @@ static int xlnx_nbputstr(const char *str) {
 	return xlnx_nbputchar('\n');
 }
 
+extern unsigned short bss_start,bss_end;
+char *commandline = "console=ttyS0 rdinit=/sbin/init";
+
 /* mark as section .img.main.text, to be referenced in linker script */
 int __attribute__ ((__section__ (".img.main.text"))) __main(void) {
-	void (*kernel)();
+
+        char *c;
+	void (*kernel)(char *, unsigned long, char*);
+
+        /* clear bss */
+        c = &bss_start;
+        while (c < &bss_end) {
+                *c=0;
+                c++;
+        }
 
 	/* call kernel */
-	kernel = (void (*)()) CONFIG_KERNEL_BASE_ADDR;
+	kernel = (void (*)(char *, unsigned long, char*)) CONFIG_KERNEL_BASE_ADDR;
 	xlnx_nbputstr("Call kernel");
-	kernel();
+	/*
+         * Linux Kernel Parameters (passing device tree):
+         * r5: pointer to command line
+         * r6: pointer to ramdisk
+         * r7: pointer to the fdt, followed by the board info data
+         */
+	kernel(commandline, 0, NULL);
 	return 0;
 }
 
