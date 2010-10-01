a48bd741f9e1ba0e37f7ca5573c18d9749b3b1ff
anton.bondarev1978 <anton.bondarev1978@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-10-01 11:53:53 +0000

    review lego-nxt platform

--- embox/platform/lego_nxt/hardware_init_hook.c
+++ embox/platform/lego_nxt/hardware_init_hook.c
@@ -1,24 +1,14 @@
-/**
- * @file arch.c
- * @brief ARM basic arch work 
- *
- * @date 21.06.2010 
- * @author Anton Kozlov 
- */
-
 #include <hal/reg.h>
-#include <drivers/watchdog.h>
 
 #include <drivers/at91sam7s256.h>
 
-#if 0
-static void initialize_main_clock() {
+void hardware_init_hook(void) {
 	REG_STORE(AT91C_CKGR_MOR, AT91C_CKGR_MOSCEN | 6 << 8 );
 	while (! (REG_LOAD(AT91C_PMC_SR) | AT91C_PMC_MOSCS));
 
 	REG_STORE(AT91C_CKGR_PLLR, (CONFIG_SYS_CLK_MUL << AT91C_CKGR_MUL_OFFSET) | \
 							   (CONFIG_SYS_CLK_DIV << AT91C_CKGR_DIV_OFFSET) | \
-							   (0x1c << AT91C_CKGR_PLLCOUNT_OFFSET)); 
+							   (0x1c << AT91C_CKGR_PLLCOUNT_OFFSET));
 	while (! (REG_LOAD(AT91C_PMC_SR) | AT91C_PMC_LOCK));
 
 	REG_STORE(AT91C_PMC_MCKR, AT91C_PMC_PRES_CLK_2);
@@ -26,19 +16,4 @@ static void initialize_main_clock() {
 
 	REG_STORE(AT91C_PMC_MCKR, AT91C_PMC_CSS_PLL_CLK | AT91C_PMC_PRES_CLK_2);
 	while (! (REG_LOAD(AT91C_PMC_SR) | AT91C_PMC_MCKRDY));
-}
-#endif
-
-void arch_init(void) {
-#if 0
-	watchdog_disable();
-	initialize_main_clock();
-#endif
-}
-
-void arch_idle(void) {
-}
-
-void arch_shutdown(void) {
-	while(1);
 }
