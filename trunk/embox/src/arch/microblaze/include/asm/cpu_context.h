78946c911064319191fe7edb44dcaf79f2a3c984
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-10-22 22:15:52 +0000

    apply some tools for auto-formatting(sed 's/\r//' and so on), fix codestyle. (logic not changed)

--- trunk/embox/src/arch/microblaze/include/asm/cpu_context.h
+++ trunk/embox/src/arch/microblaze/include/asm/cpu_context.h
@@ -1,27 +1,28 @@
 /**
- * \file cpu_context.h
- *
- * \brief Defines CPU context structure and offsets
+ * @file
+ * @brief Defines CPU context structure and offsets
  *	TODO comments = shit
- * \date Jan 26, 2009
- * \author Eldar Abusalimov
+ *      Maybe remove it? (sikmir)
+ * @date Jan 26, 2009
+ * @author Eldar Abusalimov
  */
 
-#ifndef CPU_CONTEXT_H_
-#define CPU_CONTEXT_H_
-
+#ifndef MICROBLAZE_CPU_CONTEXT_H_
+#define MICROBLAZE_CPU_CONTEXT_H_
 
 #ifndef __ASSEMBLER__
-#define ICEBRICK_REGISTERS_QUANTITY 0x10
+
+#define MICROBLAZE_REGISTERS_QUANTITY 0x10
 /*
  * Structure holding the whole context of the CPU
  */
 typedef struct _CPU_CONTEXT {
-	unsigned int regs[ICEBRICK_REGISTERS_QUANTITY];
+	unsigned int regs[MICROBLAZE_REGISTERS_QUANTITY];
 	unsigned int pcs[2];
 }__attribute__ ((aligned (8))) CPU_CONTEXT;
 
 #else /* __ASSEMBLER__ */
 
 #endif /* __ASSEMBLER__ */
-#endif /* CPU_CONTEXT_H_ */
+
+#endif /* MICROBLAZE_CPU_CONTEXT_H_ */
