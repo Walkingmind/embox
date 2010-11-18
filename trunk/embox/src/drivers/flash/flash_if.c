f0b878b3eb033197ebdeded376dc3aa7d3c4dc09
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-11-18 10:51:45 +0000

    [build] fix some problem with build

--- trunk/embox/src/drivers/flash/flash_if.c
+++ trunk/embox/src/drivers/flash/flash_if.c
@@ -12,12 +12,13 @@
 
 EMBOX_UNIT_INIT(unit_init);
 
+#define __ATTR __attribute__ ((section (".reloc.flash_utils")))
 
-int flash_if_read_id(uint32_t address, uint32_t *mcode, uint32_t *device_id)
-								__attribute__ ((section (".reloc.flash_utils")));
+FLASH_DEV flash_devices_table[MAX_FLASH_DEVS];
+
+int flash_if_read_id(uint32_t address, uint32_t *mcode, uint32_t *device_id) __ATTR;
 /*
-int flash_if_write(FLASH_DEV *flash_dev, uint32_t addr, void *buff, uint32_t size)
-								__attribute__ ((section (".reloc.flash_utils")));
+int flash_if_write(FLASH_DEV *flash_dev, uint32_t addr, void *buff, uint32_t size) __ATTR;
 */
 
 /**
@@ -55,7 +56,7 @@ int flash_if_read_id(uint32_t address, uint32_t *mcode, uint32_t *device_id) {
 }
 
 static int flash_alloc_slot(int flash_device_id) {
-	int i;
+	size_t i;
 	for (i = 0; i < MAX_FLASH_DEVS; i++) {
 		if (flash_devices_table[i].number == 0) {
 			flash_devices_table[i].number = flash_device_id;
@@ -79,75 +80,72 @@ static int flash_if_device_init(uint32_t address, int num) {
 	flash_devices_table[slot_number].start_address = address;
 	flash_devices_table[slot_number].number = num;
 
-	switch (device_id)
-		{
-			case 0x891C891C:
-				printf("Intel P30 bottom\n");
-				flash_devices_table[slot_number].erase_all_blocks = flash_erase_all_blocks;
-				flash_devices_table[slot_number].erase_block = flash_erase_block;
-				flash_devices_table[slot_number].get_block_offset = flash_get_block_offset;
-				flash_devices_table[slot_number].lock_block = flash_lock_block;
-				flash_devices_table[slot_number].lock_block_down = flash_lock_block_down;
-				flash_devices_table[slot_number].program_flash = flash_program_flash;
-				flash_devices_table[slot_number].program_flash_buffered = flash_program_flash_buffered;
-				flash_devices_table[slot_number].query = flash_query;
-				flash_devices_table[slot_number].read_block_status = flash_read_block_status;
-				flash_devices_table[slot_number].read_device_id = flash_read_device_ID;
-				flash_devices_table[slot_number].read_flash = flash_read_flash;
-				flash_devices_table[slot_number].read_status = flash_read_status;
-				flash_devices_table[slot_number].lock_all_blocks = flash_lock_all_blocks;
-				flash_devices_table[slot_number].unlock_all_blocks = flash_unlock_all_blocks;
-				flash_devices_table[slot_number].unlock_block = flash_unlock_block;
-				flash_devices_table[slot_number].param_block_nums[0] = 0;
-				flash_devices_table[slot_number].param_block_nums[1] = 1;
-				flash_devices_table[slot_number].param_block_nums[2] = 2;
-				flash_devices_table[slot_number].param_block_nums[3] = 3;
-				flash_devices_table[slot_number].is_top = 0;
-				flash_devices_table[slot_number].param_block_size = FLASH_PARM_BLOCK_NUMBYTES;
-				flash_devices_table[slot_number].main_block_size = FLASH_MAIN_BLOCK_NUMBYTES;
+	switch (device_id) {
+	case 0x891C891C:
+		printf("Intel P30 bottom\n");
+		flash_devices_table[slot_number].erase_all_blocks = flash_erase_all_blocks;
+		flash_devices_table[slot_number].erase_block = flash_erase_block;
+		flash_devices_table[slot_number].get_block_offset = flash_get_block_offset;
+		flash_devices_table[slot_number].lock_block = flash_lock_block;
+		flash_devices_table[slot_number].lock_block_down = flash_lock_block_down;
+		flash_devices_table[slot_number].program_flash = flash_program_flash;
+		flash_devices_table[slot_number].program_flash_buffered = flash_program_flash_buffered;
+		flash_devices_table[slot_number].query = flash_query;
+		flash_devices_table[slot_number].read_block_status = flash_read_block_status;
+		flash_devices_table[slot_number].read_device_id = flash_read_device_ID;
+		flash_devices_table[slot_number].read_flash = flash_read_flash;
+		flash_devices_table[slot_number].read_status = flash_read_status;
+		flash_devices_table[slot_number].lock_all_blocks = flash_lock_all_blocks;
+		flash_devices_table[slot_number].unlock_all_blocks = flash_unlock_all_blocks;
+		flash_devices_table[slot_number].unlock_block = flash_unlock_block;
+		flash_devices_table[slot_number].param_block_nums[0] = 0;
+		flash_devices_table[slot_number].param_block_nums[1] = 1;
+		flash_devices_table[slot_number].param_block_nums[2] = 2;
+		flash_devices_table[slot_number].param_block_nums[3] = 3;
+		flash_devices_table[slot_number].is_top = 0;
+		flash_devices_table[slot_number].param_block_size = FLASH_PARM_BLOCK_NUMBYTES;
+		flash_devices_table[slot_number].main_block_size = FLASH_MAIN_BLOCK_NUMBYTES;
 
-				flash_devices_table[slot_number].total_numblocks = FLASH_TOTAL_NUMBLOCKS;
-				flash_devices_table[slot_number].total_size = FLASH_TOTAL_SIZE;
-				flash_devices_table[slot_number].buffer_size = FLASH_BUFFER_SIZE;
-				break;
-			case 0x89198919:
-				printf("Intel P30 top\n");
-				flash_devices_table[slot_number].erase_all_blocks = flash_erase_all_blocks;
-				flash_devices_table[slot_number].erase_block = flash_erase_block;
-				flash_devices_table[slot_number].get_block_offset = flash_get_block_offset;
-				flash_devices_table[slot_number].lock_block = flash_lock_block;
-				flash_devices_table[slot_number].lock_block_down = flash_lock_block_down;
-				flash_devices_table[slot_number].program_flash = flash_program_flash;
-				flash_devices_table[slot_number].program_flash_buffered = flash_program_flash_buffered;
-				flash_devices_table[slot_number].query = flash_query;
-				flash_devices_table[slot_number].read_block_status = flash_read_block_status;
-				flash_devices_table[slot_number].read_device_id = flash_read_device_ID;
-				flash_devices_table[slot_number].read_flash = flash_read_flash;
-				flash_devices_table[slot_number].read_status = flash_read_status;
-				flash_devices_table[slot_number].lock_all_blocks = flash_lock_all_blocks;
-				flash_devices_table[slot_number].unlock_all_blocks = flash_unlock_all_blocks;
-				flash_devices_table[slot_number].unlock_block = flash_unlock_block;
-				flash_devices_table[slot_number].param_block_nums[0] = FLASH_TOTAL_NUMBLOCKS - 4;
-				flash_devices_table[slot_number].param_block_nums[1] = FLASH_TOTAL_NUMBLOCKS - 3;
-				flash_devices_table[slot_number].param_block_nums[2] = FLASH_TOTAL_NUMBLOCKS - 2;
-				flash_devices_table[slot_number].param_block_nums[3] = FLASH_TOTAL_NUMBLOCKS - 1;
-				flash_devices_table[slot_number].is_top = 1;
-				flash_devices_table[slot_number].param_block_size = FLASH_PARM_BLOCK_NUMBYTES;
-				flash_devices_table[slot_number].main_block_size = FLASH_MAIN_BLOCK_NUMBYTES;
-
-				flash_devices_table[slot_number].total_numblocks = FLASH_TOTAL_NUMBLOCKS;
-				flash_devices_table[slot_number].total_size = FLASH_TOTAL_SIZE;
-				flash_devices_table[slot_number].buffer_size = FLASH_BUFFER_SIZE;
-				break;
-			default:
-				printf("unknown flash device, device_id = 0x%x\n", device_id);
-				break;
-		}
+		flash_devices_table[slot_number].total_numblocks = FLASH_TOTAL_NUMBLOCKS;
+		flash_devices_table[slot_number].total_size = FLASH_TOTAL_SIZE;
+		flash_devices_table[slot_number].buffer_size = FLASH_BUFFER_SIZE;
+		break;
+	case 0x89198919:
+		printf("Intel P30 top\n");
+		flash_devices_table[slot_number].erase_all_blocks = flash_erase_all_blocks;
+		flash_devices_table[slot_number].erase_block = flash_erase_block;
+		flash_devices_table[slot_number].get_block_offset = flash_get_block_offset;
+		flash_devices_table[slot_number].lock_block = flash_lock_block;
+		flash_devices_table[slot_number].lock_block_down = flash_lock_block_down;
+		flash_devices_table[slot_number].program_flash = flash_program_flash;
+		flash_devices_table[slot_number].program_flash_buffered = flash_program_flash_buffered;
+		flash_devices_table[slot_number].query = flash_query;
+		flash_devices_table[slot_number].read_block_status = flash_read_block_status;
+		flash_devices_table[slot_number].read_device_id = flash_read_device_ID;
+		flash_devices_table[slot_number].read_flash = flash_read_flash;
+		flash_devices_table[slot_number].read_status = flash_read_status;
+		flash_devices_table[slot_number].lock_all_blocks = flash_lock_all_blocks;
+		flash_devices_table[slot_number].unlock_all_blocks = flash_unlock_all_blocks;
+		flash_devices_table[slot_number].unlock_block = flash_unlock_block;
+		flash_devices_table[slot_number].param_block_nums[0] = FLASH_TOTAL_NUMBLOCKS - 4;
+		flash_devices_table[slot_number].param_block_nums[1] = FLASH_TOTAL_NUMBLOCKS - 3;
+		flash_devices_table[slot_number].param_block_nums[2] = FLASH_TOTAL_NUMBLOCKS - 2;
+		flash_devices_table[slot_number].param_block_nums[3] = FLASH_TOTAL_NUMBLOCKS - 1;
+		flash_devices_table[slot_number].is_top = 1;
+		flash_devices_table[slot_number].param_block_size = FLASH_PARM_BLOCK_NUMBYTES;
+		flash_devices_table[slot_number].main_block_size = FLASH_MAIN_BLOCK_NUMBYTES;
+		flash_devices_table[slot_number].total_numblocks = FLASH_TOTAL_NUMBLOCKS;
+		flash_devices_table[slot_number].total_size = FLASH_TOTAL_SIZE;
+		flash_devices_table[slot_number].buffer_size = FLASH_BUFFER_SIZE;
+		break;
+	default:
+		printf("unknown flash device, device_id = 0x%x\n", device_id);
+		break;
+	}
 	return slot_number;
 }
 
-int flash_if_get_block_size(FLASH_DEV *flash_dev, uint32_t blocknum)
-				__attribute__ ((section (".reloc.flash_utils")));
+int flash_if_get_block_size(FLASH_DEV *flash_dev, uint32_t blocknum) __ATTR;
 
 int flash_if_get_block_size(FLASH_DEV *flash_dev, uint32_t blocknum) {
 	if (blocknum > flash_dev->total_numblocks) {
@@ -164,8 +162,7 @@ int flash_if_get_block_size(FLASH_DEV *flash_dev, uint32_t blocknum) {
 	return flash_dev->main_block_size;
 }
 
-int flash_if_get_start_block(FLASH_DEV *flash_dev, uint32_t addr)
-					__attribute__ ((section (".reloc.flash_utils")));
+int flash_if_get_start_block(FLASH_DEV *flash_dev, uint32_t addr) __ATTR;
 
 int flash_if_get_start_block(FLASH_DEV *flash_dev, uint32_t addr) {
 	size_t i;
@@ -185,7 +182,7 @@ int flash_if_get_start_block(FLASH_DEV *flash_dev, uint32_t addr) {
 }
 
 static int unit_init() {
-	int i;
+	size_t i;
 	for (i = 0; i < MAX_FLASH_DEVS; i++) {
 		flash_devices_table[i].dev = -1;
 	}
@@ -197,8 +194,7 @@ static int unit_init() {
 
 static uint32_t flash_buff[0x20000];
 
-int flash_if_write(FLASH_DEV *flash_dev, uint32_t addr, void *buff, uint32_t size)
-					__attribute__ ((section (".reloc.flash_utils")));
+int flash_if_write(FLASH_DEV *flash_dev, uint32_t addr, void *buff, uint32_t size) __ATTR;
 
 int flash_if_write(FLASH_DEV *flash_dev, uint32_t addr, void *buff, uint32_t size) {
 	uint32_t block_num;
