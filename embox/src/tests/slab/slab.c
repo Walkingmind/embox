749816befe9d5a8c1cf3b2ad302e29bcca2a3097
alexkalmuk <alexkalmuk@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2010-12-05 16:48:10 +0000

    Changes in slab.c. Rewrote test. Add macros to create cache.

--- embox/src/tests/slab/slab.c
+++ embox/src/tests/slab/slab.c
@@ -1,70 +1,68 @@
 /**
  * @file
  * @brief Test slab allocator
- *
- * @date 28.11.10
  * @author Dmitry Zubarevich
  */
 
 #include <stdio.h>
+
 #include <embox/test.h>
 #include <lib/list.h>
 #include <kernel/mm/slab.h>
 
-EMBOX_TEST(run);
+EMBOX_TEST(run)
+;
+
+/**
+ * The test itself.
+ *
+ * @return the test result
+ * @retval 0 on success
+ * @retval nonzero on failure
+ */
+
+struct list {
+	struct list_head *next, *prev;
+	int p;
+};
+
+static LIST_HEAD(int_list);
 
 static int run(void) {
 	int result = 0;
+	struct list_head *tmp_h;
+	int i;
+	int *ptr_array;
+	ptr_array = (int*) malloc(sizeof(int) * 10);
+	ADD_CACHE(cache,int,0x100);
 
-	int *obj_1_1;
-	int *obj_1_2;
-	int *obj_1_3;
-	int *obj_1_4;
+	TRACE("\t\t = Allocate list =\n");
+	for (i = 0; i <= 10; i++) {
+		ptr_array[i] = (int*) kmem_cache_alloc(&cache);
+		if (ptr_array[i] == 0) {
+			return -1;
+		}
+		TRACE("%d object is allocated\n",i);
+	}
+	i = 0;
+	TRACE("\n\n\t\t = Test free =\n");
+	for (i = 0; i <= 10; i++) {
+		kmem_cache_free(&cache, (void*) ptr_array[i]);
+		TRACE("%d object is freed\n",i);
+	}
+	i = 0;
+	list_for_each(tmp_h, &((&cache)->obj_ptr)) {
+		i++;
+	}
 
-	double *obj_2_1;
-	double *obj_2_2;
-	double *obj_2_3;
-	double *obj_2_4;
+	if (i != (&cache)->num) {
+		TRACE("Error: no all are freed!");
+		return -1;
+	}
 
-	char *obj_3_1;
-	char *obj_3_2;
-	char *obj_3_3;
-	char *obj_3_4;
-
-	kmem_cache_t* cache1 = get_cache("cache1");
-	kmem_cache_t* cache2 = get_cache("cache2");
-	kmem_cache_t* cache3 = get_cache("cache3");
-
-	obj_1_1 = (int*) kmem_cache_alloc(cache1);
-	obj_1_2 = (int*) kmem_cache_alloc(cache1);
-	obj_1_3 = (int*) kmem_cache_alloc(cache1);
-	obj_1_4 = (int*) kmem_cache_alloc(cache1);
-
-	/* WARNING: be carefull, double type maybe not supported */
-	obj_2_1 = (double*) kmem_cache_alloc(cache2);
-	obj_2_2 = (double*) kmem_cache_alloc(cache2);
-	obj_2_3 = (double*) kmem_cache_alloc(cache2);
-	obj_2_4 = (double*) kmem_cache_alloc(cache2);
-
-	obj_3_1 = (char*) kmem_cache_alloc(cache3);
-	obj_3_2 = (char*) kmem_cache_alloc(cache3);
-	obj_3_3 = (char*) kmem_cache_alloc(cache3);
-	obj_3_4 = (char*) kmem_cache_alloc(cache3);
-
-	kmem_cache_free(cache1, obj_1_1);
-	kmem_cache_free(cache1, obj_1_2);
-	kmem_cache_free(cache1, obj_1_3);
-	kmem_cache_free(cache1, obj_1_4);
-
-	kmem_cache_free(cache2, obj_2_1);
-	kmem_cache_free(cache2, obj_2_2);
-	kmem_cache_free(cache2, obj_2_3);
-	kmem_cache_free(cache2, obj_2_4);
-
-	kmem_cache_free(cache3, obj_3_1);
-	kmem_cache_free(cache3, obj_3_2);
-	kmem_cache_free(cache3, obj_3_3);
-	kmem_cache_free(cache3, obj_3_4);
+	free(ptr_array);
+	TRACE("\ntest ");
 
 	return result;
 }
+
