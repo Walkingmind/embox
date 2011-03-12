3208975e7357f85ee9683a7c89fd6175278fcfe5
sikmir <sikmir@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-03-12 23:12:37 +0000

    autoconvert cmds

---
+++ trunk/embox/src/cmds/lspart.c
@@ -0,0 +1,43 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 20.09.2010
+ * @author Batyukov Alexander
+ */
+#include <embox/cmd.h>
+#include <getopt.h>
+#include <fs/partition.h>
+
+EMBOX_CMD(exec);
+
+static void print_usage(void) {
+	printf("Usage: lspart [-h]\n");
+}
+
+static int exec(int argsc, char **argsv) {
+	int nextOption;
+	getopt_init();
+	do {
+		nextOption = getopt(argsc, argsv, "h");
+		switch(nextOption) {
+		case 'h':
+			print_usage();
+			return 0;
+		case -1:
+			break;
+		default:
+			return 0;
+		}
+	} while (-1 != nextOption);
+
+	fs_print_all_partitions();
+
+	/* for debugging */
+/*
+	TRACE("\n\n");
+	fs_add_new_partition("user_partition_1", 0x40000, 0x20000, 1);
+	fs_print_all_partitions();
+*/
+	return 0;
+}
---
+++ trunk/embox/src/cmds/store.em
@@ -0,0 +1,19 @@
+
+$_MODS += store
+$_SRCS-store += store.c
+$_DEPS-store += embox.driver.flash
+
+$_BRIEF-store = Store memory area to flash
+
+define $_DETAILS-store
+	NAME
+		store - store memory area to flash
+	SYNOPSIS
+		store [-h] -f <src_addr> -t <dst_addr> -n <numbytes>
+	DESCRIPTION
+		-f <src_addr>
+		-t <dst_addr>
+		-n <numbytes>
+	AUTHORS
+		Alexander Batyukov
+endef
