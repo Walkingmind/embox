da0c26cf77a889fc70c576d8a7d5437e858436b4
Ignwah <Ignwah@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-07-15 07:43:48 +0000

    prepare to merge

--- branches/embox-timers/src/include/kernel/timer.h
+++ branches/embox-timers/src/include/kernel/timer.h
@@ -20,14 +20,6 @@ typedef sys_tmr_t *sys_tmr_ptr;
 
 typedef void (*TIMER_FUNC)(sys_tmr_ptr timer, void *param);
 
-/** 
- * timer type is timer_t
- */
-
-/**
- * Timer functor type is TIMER_FUNC 
- */
-
 /**
  * Initialization of the timers subsystem.
  * int timer_init(void);
