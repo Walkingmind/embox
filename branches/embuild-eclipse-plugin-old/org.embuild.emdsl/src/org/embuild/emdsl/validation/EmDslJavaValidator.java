d875b7f49a88159f78677513a7b6fc813d24ce88
Kramar.Alina <Kramar.Alina@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-07-17 16:31:16 +0000

    Add rule for file name

--- branches/embuild-eclipse-plugin-old/org.embuild.emdsl/src/org/embuild/emdsl/validation/EmDslJavaValidator.java
+++ branches/embuild-eclipse-plugin-old/org.embuild.emdsl/src/org/embuild/emdsl/validation/EmDslJavaValidator.java
@@ -1,44 +1,81 @@
 package org.embuild.emdsl.validation;
 
-import java.awt.List;
+import static java.util.Collections.singletonMap;
+import static org.eclipse.emf.ecore.resource.URIConverter.ATTRIBUTE_DIRECTORY;
+
 import java.io.File;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.URIConverter;
 import org.eclipse.xtext.validation.Check;
 import org.embuild.emdsl.emDsl.EmDslPackage;
-import org.embuild.emdsl.emDsl.Model;
-import org.embuild.emdsl.emDsl.Module;
+import org.embuild.emdsl.emDsl.Source;
 
 public class EmDslJavaValidator extends AbstractEmDslJavaValidator {
 
-	@Check
-	public void checkAllModulesStartsWithCapital(Module m) {
-		if (!Character.isUpperCase(m.getName().charAt(0))) {
-			warning("Name should start with a capital",
-					EmDslPackage.Literals.MODULE__NAME);
-		}
-	}
+	// @Check
+	// public void checkAllModulesStartsWithCapital(Module m) {
+	// if (!Character.isUpperCase(m.getName().charAt(0))) {
+	// warning("Name should start with a capital",
+	// EmDslPackage.Literals.MODULE__NAME);
+	// }
+	// }
+	//
+	// @Check
+	// public void checkNameAllModuleExistInFile(Package p) {
+	// EList<Entity> entities = p.getEntities();
+	// }
 
 	@Check
-	public void checkModuleNameEqualsFileName(Module m) {
-		URI uri = m.eResource().getURI();
-		String name = uri.toPlatformString(true);
-		
-		name = name.substring(name.lastIndexOf('/') + 1, name.lastIndexOf('.'));
+	public void checkSourseFileExistsInSameDir(Source s) {
+		String filename = s.getFilename();
+		// * ? [|||] dl*fk.[c|h]
 
-		if (!name.equals(m.getName())) {
-			error("Module " + m.getName() + " must be defined in its own file",
-					EmDslPackage.Literals.MODULE__NAME);
+		Resource r = s.eResource();
+		ResourceSet rSet = r.getResourceSet();
+		URIConverter uriConverter = rSet.getURIConverter();
+
+		URI uri = r.getURI();
+		uri = uri.appendSegments(new String[] { "..", filename });
+		if (!uriConverter.exists(uri, singletonMap(ATTRIBUTE_DIRECTORY, false))) {
+			warning("File " + filename + " does not exist",
+					EmDslPackage.Literals.SOURCE__FILENAME);
 		}
+
 	}
 
-	@Check
-	public void checkFileMustContainsSingleModule(Model model) {
-		if (model.getElements().isEmpty()) {
-			warning("No module definitions in file",
-					EmDslPackage.Literals.MODEL__ELEMENTS);
+	private boolean find(String name, String directory) {
+		File dir = new File(directory);
+		assert dir.exists() : "directory " + directory + " is assumed to exist";
+		if (dir.isDirectory()) {
+			File[] l = dir.listFiles();
+			for (int i = 0; i < l.length; i++) {
+				if (name.equals(l[i].getName())) {
+					return true;
+				}
+			}
 		}
+		return false;
+
 	}
-	
+
+	private boolean search(String name, File dir) {
+		if (!dir.isDirectory()) {
+			return false;
+		}
+		File[] l = dir.listFiles();
+		for (int i = 0; i < l.length; i++) {
+			File cur = l[i];
+			if (cur.getName().equals(name)) {
+				return true;
+			}
+			if (cur.isDirectory()) {
+				return search(name, l[i]);
+			}
+		}
+		return false;
+	}
+
 }
