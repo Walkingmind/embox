ffca3acd8010da9ad10148cef4906ab21ff63979
Kramar.Alina <Kramar.Alina@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-07-21 15:53:22 +0000

    Add validation rules

--- branches/embuild-eclipse-plugin-old/org.embuild.emdsl/src/org/embuild/emdsl/validation/EmDslJavaValidator.java
+++ branches/embuild-eclipse-plugin-old/org.embuild.emdsl/src/org/embuild/emdsl/validation/EmDslJavaValidator.java
@@ -3,72 +3,46 @@ package org.embuild.emdsl.validation;
 import static java.util.Collections.singletonMap;
 import static org.eclipse.emf.ecore.resource.URIConverter.ATTRIBUTE_DIRECTORY;
 
+import java.util.List;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.URIConverter;
-import org.eclipse.xtext.resource.IContainer;
-import org.eclipse.xtext.resource.IResourceDescription;
-import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.validation.Check;
 import org.embuild.emdsl.emDsl.EmDslPackage;
-//import org.embuild.emdsl.emDsl.Source;
-
-import com.google.inject.Inject;
+import org.embuild.emdsl.emDsl.Ifdef;
+import org.embuild.emdsl.emDsl.SrcsPropAssign;
 
 public class EmDslJavaValidator extends AbstractEmDslJavaValidator {
 
-	// @Check
-	// public void checkAllModulesStartsWithCapital(Module m) {
-	// if (!Character.isUpperCase(m.getName().charAt(0))) {
-	// warning("Name should start with a capital",
-	// EmDslPackage.Literals.MODULE__NAME);
-	// }
-	// }
-	//
-	// @Check
-	// public void checkNameAllModuleExistInFile(Package p) {
-	// EList<Entity> entities = p.getEntities();
-	// }
+	@Check
+	public void checkSourseFileExistsInSameDir(SrcsPropAssign s) {
+		String[] files = s.getFilenames().get(0).split(" ");
+		
+		for (String fileName : files) {
+			if (checkFileNameContainsGlob(fileName)) {
+				return;
+			}
 
-	@Inject
-	IContainer.Manager manager;
-	@Inject
-	IResourceDescriptions index;
+			Resource r = s.eResource();
+			URIConverter uriConverter = r.getResourceSet().getURIConverter();
 
-	public void listVisibleResources(Resource myResource) {
-		IResourceDescription descr = index.getResourceDescription(myResource
-				.getURI());
-		for (IContainer visibleContainer : manager.getVisibleContainers(descr,
-				index)) {
-			for (IResourceDescription visibleResourceDesc : visibleContainer
-					.getResourceDescriptions()) {
-				System.out.println(visibleResourceDesc.getURI());
+			URI uri = r.getURI();
+			uri = uri.appendSegments(new String[] { "..", fileName });
+			if (!uriConverter.exists(uri,
+					singletonMap(ATTRIBUTE_DIRECTORY, false))) {
+				warning("File " + fileName + " does not exist",
+						EmDslPackage.Literals.SRCS_PROP_ASSIGN__FILENAMES);
 			}
 		}
 	}
 
-//	@Check
-//	public void checkSourseFileExistsInSameDir(Source s) {
-//		String fileName = s.getFilename();
-//
-//		if (checkFileNameContainsGlob(fileName)) {
-//			return;
-//		}
-//		
-//		Resource r = s.eResource();
-//		listVisibleResources(r);
-//		
-//		ResourceSet rSet = r.getResourceSet();
-//		URIConverter uriConverter = rSet.getURIConverter();
-//
-//		URI uri = r.getURI();
-//		uri = uri.appendSegments(new String[] { "..", fileName });
-//		if (!uriConverter.exists(uri, singletonMap(ATTRIBUTE_DIRECTORY, false))) {
-//			warning("File " + fileName + " does not exist",
-//					EmDslPackage.Literals.SOURCE__FILENAME);
-//		}
-//	}
+	@Check
+	public void checkIfdefExist(Ifdef ifdef) {
+		warning("Ifdef considered harmful",
+				EmDslPackage.Literals.IFDEF__CONDITION);
+	}
 
 	private boolean checkFileNameContainsGlob(String fileName) {
 		char[] name = fileName.toCharArray();
