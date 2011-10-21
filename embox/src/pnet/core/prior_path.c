62f68643ff742874fa71f8911be1024dc1053147
alexkalmuk <alexkalmuk@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-10-21 11:26:13 +0000

    add node_for_each

--- embox/src/pnet/core/prior_path.c
+++ embox/src/pnet/core/prior_path.c
@@ -10,10 +10,36 @@
 #include <errno.h>
 #include <pnet/core.h>
 #include <pnet/prior_path.h>
+#include <util/array.h>
 
 //static struct pnet_path prior_table[0x10]; //TODO convert it to list or heap
 
-int path_set_prior(net_node_t node, net_prior_t prior) { //now handles only prior increment
+int path_set_prior(net_node_t node, net_prior_t prior) {
+	if (node->prior <= prior) {
+		return node_for_each_increase_prior(node, prior);
+	} else {
+		return node_for_each_increase_prior(node, prior);
+	}
+}
+
+static int node_for_each_decrease_prior(net_node_t node, net_prior_t prior) {
+	net_node_t node_child;
+	if (node != NULL) {
+		for (int i = 0; i < CHILD_CNT; i++) {
+			if (NULL != (node_child = node->children[i])) {
+				if (node_child->prior <= prior) {
+					break;
+				}
+				node_child->prior = prior;
+			}
+		}
+		for (int i = 0; i < CHILD_CNT; i++) {
+			node_for_each_decrease_prior(node->children[i], node->prior);
+		}
+	}
+}
+
+static int node_for_each_increase_prior(net_node_t node, net_prior_t prior) {
 	net_node_t cur_node = node;
 	while (cur_node != NULL) {
 		if (cur_node->prior >= prior) {
@@ -32,17 +58,17 @@ static int __net_core_receive(net_packet_t pack) {
 	if (node->proto->rx_hnd != NULL) {
 		res = node->proto->rx_hnd(pack);
 	}
-	
+
 	if (res == NET_HND_DFAULT) {
 		pack->node = pack->node->dfault;
-	} else if (res > 0 ){
+	} else if (res > 0) {
 		pack->node = pack->node->children[res];
 	}
 
 	if (res != NET_HND_SUPPRESSED) {
 		pnet_process(pack);
 	}
-	
+
 	return 0;
 }
 
@@ -53,23 +79,23 @@ static int __net_core_send(net_packet_t pack) {
 	if (node->proto->tx_hnd != NULL) {
 		res = node->proto->tx_hnd(pack);
 	}
-	
+
 	if (res == NET_HND_DFAULT) {
 		pack->node = pack->node->parent;
 		pnet_process(pack);
 	}
-	
-	return ENOERR; 
+
+	return ENOERR;
 }
 
 int pnet_process(net_packet_t pack) {
 	if (pack->dir == NET_PACKET_RX) {
 		return __net_core_receive(pack);
-	} 
+	}
 	return __net_core_send(pack);
 }
 
-struct pnet_path *pnet_get_dev_prior(struct net_device *dev){
+struct pnet_path *pnet_get_dev_prior(struct net_device *dev) {
 	return NULL;
 }
 
