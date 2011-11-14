405e94ad5d43b3ce5828fa224a354c3c8723be5d
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-11-14 14:49:22 +0000

    Nothing significant again

--- trunk/embox/mk/gold/engine.mk
+++ trunk/embox/mk/gold/engine.mk
@@ -371,7 +371,9 @@ define __gold_lex
 		$(__gold_location_reset_mk)
 	}
 
-	$(subst ./,/,$(subst . ,.,
+	$(filter-out %/2,# Omit whitespaces.
+			$(subst ./,/,$(subst . ,.,
+
 		$(__gold_location)/# Position of the first token.
 
 		$(foreach 1,$1,$(foreach a,$($g_dfa$(__gold_state__)),
@@ -411,7 +413,7 @@ define __gold_lex
 			# Position of the pending token and a symbol code.
 			$(if $(findstring $a,/1),/)/$(__gold_location)$(a:%/=%)
 		)
-	)) /0# EOF token at the end.
+	))) /0# EOF token at the end.
 endef
 
 # No error.
@@ -891,7 +893,7 @@ endef
 define __gold_parse
 	$(call __gold_expand,
 		$(call __gold_analyze,
-			$(filter-out %/2,$(__gold_lex))# Scan and omit whitespaces.
+			$(__gold_lex)
 		)
 	)
 endef
