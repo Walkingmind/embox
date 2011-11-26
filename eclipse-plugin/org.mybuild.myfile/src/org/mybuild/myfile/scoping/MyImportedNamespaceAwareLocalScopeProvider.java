a74b8e5e2e5f960f844f84133114d6932ef8af7d
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-11-26 01:30:20 +0000

    fix FQN provider

--- eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
+++ eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
@@ -5,12 +5,14 @@ import static java.util.Collections.singletonList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.naming.QualifiedName;
+import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.ImportNormalizer;
 import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider;
 import org.mybuild.myfile.myFile.Model;
 import org.mybuild.myfile.myFile.Package;
+
+import com.google.inject.Inject;
 
 /**
  * @author Eldar Abusalimov
@@ -18,17 +20,20 @@ import org.mybuild.myfile.myFile.Package;
 public class MyImportedNamespaceAwareLocalScopeProvider extends
 		ImportedNamespaceAwareLocalScopeProvider {
 
+	@Inject
+	private IQualifiedNameConverter qualifiedNameConverter;
+
 	@Override
 	protected IScope getResourceScope(Resource res, EReference reference) {
+		IScope globalScope = getGlobalScope(res, reference);
+
 		EObject context = res.getContents().get(0);
 		Package pkg = ((Model) context).getPackage();
 
-		IScope globalScope = getGlobalScope(res, reference);
-
 		if (pkg != null) {
 			ImportNormalizer ownNamespaceNormalizer = new ImportNormalizer(
-					QualifiedName.create(pkg.getName()), true,
-					isIgnoreCase(reference));
+					qualifiedNameConverter.toQualifiedName(pkg.getName()),
+					true, isIgnoreCase(reference));
 
 			globalScope = createImportScope(globalScope,
 					singletonList(ownNamespaceNormalizer), null,
