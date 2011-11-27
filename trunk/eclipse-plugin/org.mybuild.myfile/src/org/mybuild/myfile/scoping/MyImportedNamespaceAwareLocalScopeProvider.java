a922a604f70865cc6f7b2c63024350d7eb890a29
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-11-27 16:46:20 +0000

    Simplify getResourceScope a bit

--- trunk/eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
+++ trunk/eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
@@ -5,23 +5,17 @@ import static java.util.Collections.singletonList;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.ImportNormalizer;
 import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider;
 import org.mybuild.myfile.myFile.Model;
 import org.mybuild.myfile.myFile.Package;
 
-import com.google.inject.Inject;
-
 /**
  * @author Eldar Abusalimov
  */
 public class MyImportedNamespaceAwareLocalScopeProvider extends
 		ImportedNamespaceAwareLocalScopeProvider {
-
-	@Inject
-	private IQualifiedNameConverter qualifiedNameConverter;
 
 	@Override
 	protected IScope getResourceScope(Resource res, EReference reference) {
@@ -31,9 +25,8 @@ public class MyImportedNamespaceAwareLocalScopeProvider extends
 		Package pkg = ((Model) context).getPackage();
 
 		if (pkg != null) {
-			ImportNormalizer ownNamespaceNormalizer = new ImportNormalizer(
-					qualifiedNameConverter.toQualifiedName(pkg.getName()),
-					true, isIgnoreCase(reference));
+			ImportNormalizer ownNamespaceNormalizer = createImportedNamespaceResolver(
+					pkg.getName() + ".*", isIgnoreCase(reference));
 
 			globalScope = createImportScope(globalScope,
 					singletonList(ownNamespaceNormalizer), null,
@@ -41,6 +34,11 @@ public class MyImportedNamespaceAwareLocalScopeProvider extends
 		}
 
 		return getResourceScope(globalScope, context, reference);
+	}
+
+	@Override
+	protected boolean isRelativeImport() {
+		return false;
 	}
 
 }
