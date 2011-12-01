17cc887bfffd1964e0644556365194e59a8cf60a
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2011-12-01 15:10:16 +0000

    Add syntax coloring (interface features in italic)

---
+++ trunk/eclipse-plugin/org.mybuild.myfile.ui/src/org/mybuild/myfile/ui/syntaxcoloring/MyFileSemanticHighlightingCalculator.java
@@ -0,0 +1,43 @@
+package org.mybuild.myfile.ui.syntaxcoloring;
+
+import org.eclipse.xtext.CrossReference;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.nodemodel.ICompositeNode;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightedPositionAcceptor;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.ISemanticHighlightingCalculator;
+import org.mybuild.myfile.myFile.FeatureList;
+
+public class MyFileSemanticHighlightingCalculator implements
+		ISemanticHighlightingCalculator {
+
+	public void provideHighlightingFor(final XtextResource resource,
+			IHighlightedPositionAcceptor acceptor) {
+		if (resource == null || resource.getParseResult() == null)
+			return;
+
+		for (INode node : resource.getParseResult().getRootNode()
+				.getAsTreeIterable()) {
+			if (node.getGrammarElement() instanceof CrossReference
+					&& node.getSemanticElement() instanceof FeatureList) {
+				highlightFeatureRef(node, acceptor);
+			}
+		}
+	}
+
+	private void highlightFeatureRef(INode node,
+			IHighlightedPositionAcceptor acceptor) {
+		while (node instanceof ICompositeNode) {
+			node = ((ICompositeNode) node).getLastChild();
+		}
+
+		if (node == null || node.getGrammarElement() instanceof Keyword) {
+			return;
+		}
+
+		acceptor.addPosition(node.getOffset(), node.getLength(),
+				MyFileHighlightingConfiguration.FEATURE_REF);
+	}
+
+}
