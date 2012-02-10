0205e7af3f5a381ed653132308adac64b50b158d
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-02-10 13:01:39 +0000

    Second stage script loader (myfiles+linking)

--- branches/mybuild/mk/load.mk
+++ branches/mybuild/mk/load.mk
@@ -4,7 +4,7 @@
 # Author: Eldar Abusalimov
 #
 
-CACHE_DIR := mk/.cache
+MK_CACHE_DIR := mk/.cache/mk
 
 # Default values which we are about to override with target-specific ones.
 export CACHE_INCLUDES :=
@@ -19,12 +19,12 @@ export CACHE_REQUIRES :=
 export ALLOC_SCOPE
 
 # Core scripts: def & obj.
-export mk_core_def := $(CACHE_DIR)/mk_core_def.mk
+export mk_core_def := $(MK_CACHE_DIR)/mk_core_def.mk
 $(mk_core_def) : CACHE_INCLUDES := \
 	mk/core/define.mk
 $(mk_core_def) : ALLOC_SCOPE := a
 
-export mk_core_obj := $(CACHE_DIR)/mk_core_obj.mk
+export mk_core_obj := $(MK_CACHE_DIR)/mk_core_obj.mk
 $(mk_core_obj) : CACHE_INCLUDES := \
 	mk/core/object.mk
 $(mk_core_obj) : CACHE_REQUIRES := \
@@ -32,7 +32,7 @@ $(mk_core_obj) : CACHE_REQUIRES := \
 $(mk_core_obj) : ALLOC_SCOPE := b
 
 # GOLD parser engine.
-export mk_gold_engine := $(CACHE_DIR)/mk_gold_engine.mk
+export mk_gold_engine := $(MK_CACHE_DIR)/mk_gold_engine.mk
 $(mk_gold_engine) : CACHE_INCLUDES := \
 	mk/gold/engine.mk
 $(mk_gold_engine) : CACHE_REQUIRES := \
@@ -41,7 +41,7 @@ $(mk_gold_engine) : ALLOC_SCOPE := c
 
 ifeq (1,0) ###
 # Tiny version of EMF Ecore.
-export mk_model := $(CACHE_DIR)/mk_model.mk
+export mk_model := $(MK_CACHE_DIR)/mk_model.mk
 $(mk_model) : CACHE_INCLUDES := \
 	mk/model/model.mk     \
 	mk/model/factory.mk   \
@@ -53,7 +53,7 @@ $(mk_model) : CACHE_REQUIRES := \
 $(mk_model) : ALLOC_SCOPE := d
 
 # Mybuild itself.
-export mk_mybuild := $(CACHE_DIR)/mk_mybuild.mk
+export mk_mybuild := $(MK_CACHE_DIR)/mk_mybuild.mk
 $(mk_mybuild) : CACHE_INCLUDES := \
 	mk/mybuild/myfile-model.mk     \
 	mk/mybuild/myfile-factory.mk   \
@@ -75,11 +75,12 @@ all_mk_scripts := \
 
 else ###
 
-export mk_mybuild := $(CACHE_DIR)/mk_mybuild.mk
+export mk_mybuild := $(MK_CACHE_DIR)/mk_mybuild.mk
 $(mk_mybuild) : CACHE_INCLUDES := \
 	mk/mybuild/model.mk     \
-	mk/mybuild/myfile.mk     \
-	mk/mybuild/resource.mk
+	mk/mybuild/myfile.mk    \
+	mk/mybuild/resource.mk  \
+	mk/mybuild/mybuild.mk
 $(mk_mybuild) : CACHE_REQUIRES := \
 	$(mk_core_obj) \
 	$(mk_gold_engine)
@@ -94,7 +95,7 @@ all_mk_scripts := \
 endif ###
 
 $(MAKECMDGOALS) : $(all_mk_scripts)
-	@$(MAKE) -f mk/main.mk MAKEFILES=$(mk_mybuild) $@
+	@$(MAKE) -f mk/load2.mk $@
 
 -include $(all_mk_scripts:%=%.d)
 
@@ -104,6 +105,6 @@ $(all_mk_scripts) : $$(CACHE_REQUIRES)
 $(all_mk_scripts) : mk/load.mk
 $(all_mk_scripts) : mk/script/mk-cache.mk
 $(all_mk_scripts) :
-	@echo Preparing $(@F)...
+	@echo ' MKGEN $(@F)'
 	@mkdir -p $(@D) && \
 		$(MAKE) -f mk/script/mk-cache.mk CACHE_DEP_TARGET='$@' > $@
