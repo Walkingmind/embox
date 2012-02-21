33eba843d1f24477d87adcb77d0f1f43dcbe4593
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-02-21 21:03:52 +0000

    Avoid separating model and its impl, remove factory

--- embox/mk/model/resource.mk
+++ embox/mk/model/resource.mk
@@ -28,6 +28,9 @@ define class-Resource
 	$(setter rootObject,
 		$(assert $(not $(multiword $1)))
 
+		# Reset file name.
+		$(set-field fileName,)
+
 		$(for oldRootObject <- $(get-field rootObject),
 			$(set-field oldRootObject->__eContainer,))
 
@@ -68,8 +71,8 @@ define class-Resource
 	# Loads the resource from the specified location.
 	#   1. File name.
 	$(method load,
-		$(set-field fileName,$1)
-		$(set rootObject,$(invoke loadRootObject,$1)))
+		$(set rootObject,$(invoke loadRootObject,$1))
+		$(set-field fileName,$1))
 
 	# Implementation have to retrieve the root object of this resource.
 	#   1. File name.
@@ -137,7 +140,7 @@ define class-UnresolvedLinkIssue
 	$(getter severity,
 		error)
 	$(getter location,
-		0:0)
+		$(get $(get link).origin))
 	$(getter message,
 		$(for link     <- $(get link),
 			targetType <- $(get $(get link->eMetaReference).eReferenceType),
