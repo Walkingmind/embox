8620cfe96c0814efd179f5d5acb7d411127c0261
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-03-16 14:32:54 +0000

    [mybuild] resource/linker interworking changed

--- branches/mybuild/mk/model/model_impl.mk
+++ branches/mybuild/mk/model/model_impl.mk
@@ -334,7 +334,7 @@ define __eObjectResolveLinks
 			$(warning on-demand linkage: \
 				[$(get link->eMetaReference)] '$(get link->name)'))
 		$(invoke $(get resourceSet->linker).resolveLinksGroup,
-			$(subst ./,,$1),$2)
+			$(subst ./,,$1),$($2))
 	),)
 endef
 
