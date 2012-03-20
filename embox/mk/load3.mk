ea287e0ec50388e48889b3bebbdccfb0697eb756
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-03-20 15:05:54 +0000

    oops, fixed building

--- embox/mk/load3.mk
+++ embox/mk/load3.mk
@@ -10,7 +10,7 @@ export CONFIGFILES_CACHE_DIR := $(MYBUILD_CACHE_DIR)/config
 
 CONFIG_PATH := conf/
 
-CONFIG_GENERATED := $(CONFIG_PATH)/genConf.config
+CONFIG_GENERATED := $(CONFIG_PATH)genConf.config
 
 $(CONFIG_GENERATED) : 
 	mkdir -p $(@D)
@@ -35,7 +35,7 @@ $(MAKECMDGOALS) : $(configfiles_linked_mk)
 
 .DELETE_ON_ERROR:
 
-.PHONY : $(configfiles_linked_mk) $(CONFIG_GENERATED)
+.PHONY : $(configfiles_linked_mk) 
 
 HOSTCPP = gcc -E
 
