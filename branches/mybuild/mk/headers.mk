7099f95bcfbe24dd8f8efdfeed18491bfa04875e
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-03-20 11:19:30 +0000

    kill mybuild branch

--- branches/mybuild/mk/headers.mk
+++
@@ -1,7 +0,0 @@
-
-# By module get header
-HEADERS_BUILD := \
-  $(patsubst %,$(OBJ_DIR)/mods/%.h,$(subst .,/,$(basename $(APIS_BUILD))))
-
-$(HEADERS_BUILD): mk/image.mk $(AUTOCONF_DIR)/mods.mk
-	@$(MKDIR) $(@D) && printf "%b" '$(__header_gen)' > $@
