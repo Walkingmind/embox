df5d7ceb14c9d64af88e6e9a354a0cc3a7f3f79b
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-03-29 15:31:50 +0000

    add implicit import of mybuild.lang.*

--- trunk/eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
+++ trunk/eclipse-plugin/org.mybuild.myfile/src/org/mybuild/myfile/scoping/MyImportedNamespaceAwareLocalScopeProvider.java
@@ -1,10 +1,13 @@
 package org.mybuild.myfile.scoping;
 
-import static java.util.Collections.singletonList;
+import static com.google.common.collect.Lists.newLinkedList;
+
+import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.ImportNormalizer;
 import org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider;
@@ -20,19 +23,20 @@ public class MyImportedNamespaceAwareLocalScopeProvider extends
 	@Override
 	protected IScope getResourceScope(Resource res, EReference reference) {
 		IScope globalScope = getGlobalScope(res, reference);
+		List<ImportNormalizer> normalizers = newLinkedList();
 
 		EObject context = res.getContents().get(0);
 		Package pkg = ((Model) context).getPackage();
 
 		if (pkg != null) {
-			ImportNormalizer ownNamespaceNormalizer = createImportedNamespaceResolver(
-					pkg.getName() + ".*", isIgnoreCase(reference));
-
-			globalScope = createImportScope(globalScope,
-					singletonList(ownNamespaceNormalizer), null,
-					reference.getEReferenceType(), isIgnoreCase(reference));
+			normalizers.add(createImportedNamespaceResolver(pkg.getName()
+					+ ".*", isIgnoreCase(reference)));
 		}
+		normalizers.add(new ImportNormalizer(QualifiedName.create("mybuild",
+				"lang"), true, false));
 
+		globalScope = createImportScope(globalScope, normalizers, null,
+				reference.getEReferenceType(), isIgnoreCase(reference));
 		return getResourceScope(globalScope, context, reference);
 	}
 
