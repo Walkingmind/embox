d7f40d6cab2db8e8cbddc18021571f81e901be35
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-03-30 14:32:34 +0000

    Syntax coloring for annotations

--- eclipse-plugin/org.mybuild.myfile.ui/src/org/mybuild/myfile/ui/syntaxcoloring/MyFileHighlightingConfiguration.java
+++ eclipse-plugin/org.mybuild.myfile.ui/src/org/mybuild/myfile/ui/syntaxcoloring/MyFileHighlightingConfiguration.java
@@ -1,6 +1,8 @@
 package org.mybuild.myfile.ui.syntaxcoloring;
 
+import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
 import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
 import org.eclipse.xtext.ui.editor.utils.TextStyle;
@@ -12,18 +14,36 @@ import org.eclipse.xtext.ui.editor.utils.TextStyle;
 public class MyFileHighlightingConfiguration extends
 		DefaultHighlightingConfiguration {
 
-	public final static String FEATURE_REF = "FeatureReference";
+	public final static String FEATURE_REFERENCE = "mybuild.FeatureReference";
+	public final static String ANNOTATION_TYPE_REFERENCE = "mybuild.AnnotationTypeReference";
+	public static final String DEPRECATED = "mybuild.Deprecated";
 
 	@Override
 	public void configure(IHighlightingConfigurationAcceptor acceptor) {
 		super.configure(acceptor);
-		acceptor.acceptDefaultHighlighting(FEATURE_REF, "Feature References",
-				featureReferenceTextStyle());
+		acceptor.acceptDefaultHighlighting(FEATURE_REFERENCE,
+				"Feature Reference", featureReferenceTextStyle());
+		acceptor.acceptDefaultHighlighting(ANNOTATION_TYPE_REFERENCE,
+				"Annotation", annotationTypeReferenceTextStyle());
+		acceptor.acceptDefaultHighlighting(DEPRECATED, "Deprecated",
+				deprecated());
 	}
 
 	public TextStyle featureReferenceTextStyle() {
 		TextStyle textStyle = defaultTextStyle().copy();
 		textStyle.setStyle(SWT.ITALIC);
+		return textStyle;
+	}
+
+	public TextStyle annotationTypeReferenceTextStyle() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setColor(new RGB(100, 100, 100));
+		return textStyle;
+	}
+
+	public TextStyle deprecated() {
+		TextStyle textStyle = defaultTextStyle().copy();
+		textStyle.setStyle(TextAttribute.STRIKETHROUGH);
 		return textStyle;
 	}
 }
