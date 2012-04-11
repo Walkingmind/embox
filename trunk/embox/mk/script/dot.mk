31ee6254670213b83e3a7714b99a95f1c07f594e
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-04-11 16:20:52 +0000

    rename check-make-flags.mk into script-common.mk and move common 'all' goal definition there

--- trunk/embox/mk/script/dot.mk
+++ trunk/embox/mk/script/dot.mk
@@ -4,11 +4,7 @@
 # Author: Anton Kozlov 
 #
 
-.PHONY : all
-all :
-	@#
-
-include mk/script/check-make-flags.mk
+include mk/script/script-common.mk
 
 ifndef BUILD_MODEL
 $(error BUILD_MODEL is not defined)
