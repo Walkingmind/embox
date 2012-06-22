a94ce3f2cbcc9a83a642e6af6446b29109c29545
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-06-22 15:45:28 +0000

    use recursively expanded variables in generated makefiles

--- embox/mk/script/build/build-gen.mk
+++ embox/mk/script/build/build-gen.mk
@@ -53,7 +53,7 @@ gen_banner = \
 # 1. Variable name.
 # 2. Value.
 gen_make_var = \
-	$(PRINTF) '%s := %s\n\n' \
+	$(PRINTF) '%s = %s\n\n' \
 		$(call sh_quote,$1) \
 		$(call sh_quote,$2)
 
@@ -77,7 +77,7 @@ gen_make_dep = \
 # 2. Variable name.
 # 3. Value.
 gen_make_tsvar = \
-	$(PRINTF) '%s : %s := %s\n\n' \
+	$(PRINTF) '%s : %s = %s\n\n' \
 		$(call sh_quote,$1) \
 		$(call sh_quote,$2) \
 		$(call sh_quote,$3)
@@ -86,7 +86,7 @@ gen_make_tsvar = \
 # 2. Variable name.
 # 3. Value (assumed to be a list).
 gen_make_tsvar_list = \
-	$(PRINTF) '%s : %s := $(foreach ,$3,\\\n\t\t%s)\n\n' \
+	$(PRINTF) '%s : %s = $(foreach ,$3,\\\n\t\t%s)\n\n' \
 		$(call sh_quote,$1) \
 		$(call sh_quote,$2) \
 		$(foreach v,$3,$(call sh_quote,$v))
