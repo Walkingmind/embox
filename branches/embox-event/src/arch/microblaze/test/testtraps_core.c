a30dfd91dfa0de2520fda61d9f4a96d0c936a0d7
iantonishko <iantonishko@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-09-07 13:08:34 +0000

    Branching embox-event.

---
+++ branches/embox-event/src/arch/microblaze/test/testtraps_core.c
@@ -0,0 +1,63 @@
+/**
+ * @file
+ *
+ * @date 10.03.10
+ * @author Anton Bondarev
+ */
+
+#include <types.h>
+#include <hal/test/traps_core.h>
+
+static traps_env_t test_env[1];
+
+//FIXME testtraps_set_handler haven't been implemented yet
+void testtraps_set_handler(uint32_t type, int number, trap_handler_t handler) {
+	switch(type) {
+	case TRAP_TYPE_HARDTRAP:
+		hwtrap_handler[number] = handler;
+		break;
+	case TRAP_TYPE_INTERRUPT:
+		break;
+	case TRAP_TYPE_SOFTTRAP:
+		sotftrap_handler[number] = handler;
+		break;
+	default:
+		/*ERROR: unknown type*/
+		return;
+	}
+}
+int testtraps_fire_softtrap(uint32_t number, void *data) {
+	int res;
+	/*pass trap number through r5 and data through r6*/
+#if 0
+	__asm__ __volatile__ (
+		"bralid r16, 0x8;\n\t"
+		"nop;\n\t"
+		"nop;\n\t"
+	);
+#else
+	__asm__ __volatile__ (
+		/* we have already loaded parameters
+		 "addi  r5, %0, 0;\n\t"
+		"addi  r6, %1, 0;\n\t"
+		*/
+		"brki  r16, 0x8;\n\t"
+		"nop;\n\t"
+		"nop;\n\t"
+		/*: for loaded parametrs
+		:"r" (number), "r"(data)
+		*/
+	);
+
+#endif
+	__asm__ __volatile__ (""
+			"addi %0, r3, 0\n\t"
+			: "=r" (res) : : "memory"
+			);
+
+	return res;
+}
+
+traps_env_t *testtraps_env(void) {
+	return test_env;
+}
---
+++ branches/embox-event/src/tests/hal/context/context_switch.c
@@ -0,0 +1,51 @@
+/**
+ * @file
+ * @brief Context switch test
+ *
+ * @date 20.04.10
+ * @author Eldar Abusalimov
+ */
+
+#include <hal/context.h>
+#include <embox/test.h>
+#include <stdio.h>
+
+#define STACK_SZ 0x10000
+
+EMBOX_TEST(run);
+
+static struct context entry_context, infinite_context, redundant_context;
+static char entry_stack[STACK_SZ];
+static char infinite_stack[STACK_SZ];
+
+static void entry(void) {
+	TRACE("entry begin\n");
+	while (true) {
+		TRACE("E");
+		context_switch(&entry_context, &infinite_context);
+	}
+	TRACE("entry end (should not be reached)\n");
+}
+
+static void infinite(void) {
+	while (true) {
+		TRACE("?");
+		context_switch(&infinite_context, &entry_context);
+	}
+}
+
+static int run(void) {
+	context_init(&entry_context, true);
+	context_set_entry(&entry_context, entry);
+	context_set_stack(&entry_context, entry_stack + STACK_SZ);
+
+	context_init(&infinite_context, true);
+	context_set_entry(&infinite_context, infinite);
+	context_set_stack(&infinite_context, infinite_stack + STACK_SZ);
+
+	TRACE("test begin\n");
+	context_switch(&redundant_context, &entry_context);
+	TRACE("test end\n");
+
+	return 0;
+}
