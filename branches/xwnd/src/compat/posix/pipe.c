e97ec867673f0ecca5e78f7984f5c71524fa5ad1
alexandr.chernakov@gmail.com <alexandr.chernakov@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-09-28 10:02:24 +0000

    =Creating a branch for XWnd

---
+++ branches/xwnd/src/compat/posix/pipe.c
@@ -0,0 +1,140 @@
+/**
+ * @file
+ * @brief Pipe implementation.
+ *
+ * @date 12.09.2012
+ * @author Alexander Kalmuk
+ */
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <util/async_ring_buff.h>
+#include <kernel/task.h>
+#include <kernel/task/idx.h>
+#include <framework/mod/options.h>
+
+#define PIPE_BUFFER_SIZE OPTION_GET(NUMBER, pipe_buffer_size)
+
+static int pipe_close(struct idx_desc_data *data);
+static int pipe_read(struct idx_desc_data *data, void *buf, size_t nbyte);
+static int pipe_write(struct idx_desc_data *data, const void *buf, size_t nbyte);
+static int pipe_ioctl(struct idx_desc_data *data, int request, va_list args);
+
+static const struct task_idx_ops read_ops = {
+		.read = pipe_read,
+		.close = pipe_close,
+		.ioctl = pipe_ioctl,
+		.type = TASK_RES_OPS_REGULAR
+};
+
+static const struct task_idx_ops write_ops = {
+		.write = pipe_write,
+		.close = pipe_close,
+		.ioctl = pipe_ioctl,
+		.type = TASK_RES_OPS_REGULAR
+};
+
+struct pipe {
+	struct async_ring_buff buff;
+	struct event nonfull; /* activates the writer */
+	struct event nonempty; /* activates the reader */
+	int ends_count : 2; /* 0, 1 or 2 */
+};
+
+int pipe(int pipefd[2]) {
+	struct pipe *pipe = malloc(sizeof(struct pipe));
+	struct async_ring_buff *pipe_buff = &pipe->buff;
+	void *storage = malloc(PIPE_BUFFER_SIZE);
+
+	async_ring_buff_init(pipe_buff, 1, PIPE_BUFFER_SIZE, storage);
+
+	pipe->ends_count = 2;
+	event_init(&pipe->nonfull, "pipe_is_nonfull");
+	event_init(&pipe->nonempty, "pipe_is_nonempty");
+
+	pipefd[1] = task_self_idx_alloc(&write_ops, pipe_buff);
+	if (pipefd[0] < 0) {
+		free(storage);
+		free(pipe);
+		SET_ERRNO(EMFILE);
+		return -1;
+	}
+
+	pipefd[0] = task_self_idx_alloc(&read_ops, pipe_buff);
+	if (pipefd[1] < 0) {
+		task_self_idx_table_unbind(pipefd[0]);
+		free(storage);
+		free(pipe);
+		SET_ERRNO(EMFILE);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int pipe_close(struct idx_desc_data *data) {
+	struct pipe *pipe= (struct pipe*) data->fd_struct;
+
+	if (!(--pipe->ends_count)) {
+		free(pipe->buff.buffer.storage);
+		free(pipe);
+	}
+
+	return 0;
+}
+
+static int pipe_read(struct idx_desc_data *data, void *buf, size_t nbyte) {
+	int len;
+	struct pipe *pipe = (struct pipe*)data->fd_struct;
+
+	if (!nbyte || pipe->ends_count == 1) {
+		return 0;
+	}
+
+	if (data->flags & O_NONBLOCK) {
+		len = async_ring_buff_dequeue(&pipe->buff, (void*)buf, nbyte);
+	} else {
+		while (!(len = async_ring_buff_dequeue(&pipe->buff, (void*)buf, nbyte))) {
+			event_wait(&pipe->nonempty, SCHED_TIMEOUT_INFINITE);
+		}
+	}
+
+	if (len > 0) {
+		event_notify(&pipe->nonfull);
+	}
+
+	return len;
+}
+
+static int pipe_write(struct idx_desc_data *data, const void *buf, size_t nbyte) {
+	int len;
+	struct pipe *pipe = (struct pipe*)data->fd_struct;
+
+	if (pipe->ends_count == 1) {
+		SET_ERRNO(EPIPE);
+		return -1;
+	}
+
+	if (!nbyte) {
+		return nbyte;
+	}
+
+	if (data->flags & O_NONBLOCK) {
+		len = async_ring_buff_enqueue(&pipe->buff, (void*)buf, nbyte);
+	} else {
+		while (!(len = async_ring_buff_enqueue(&pipe->buff, (void*)buf, nbyte))) {
+			event_wait(&pipe->nonfull, SCHED_TIMEOUT_INFINITE);
+		}
+	}
+
+	if (len > 0) {
+		event_notify(&pipe->nonempty);
+	}
+
+	return len;
+}
+
+static int pipe_ioctl(struct idx_desc_data *data, int request, va_list args) {
+	return 0;
+}
---
+++ branches/xwnd/src/fs/driver/fat/fatfs_subr.c
@@ -0,0 +1,119 @@
+/**
+ * @file
+ * @brief 
+ *
+ * @date 14.08.2012
+ * @author Andrey Gazukin
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fs/fat.h>
+#include <fs/node.h>
+#include <fs/vfs.h>
+
+/*
+ *	Convert a filename element from canonical (8.3) to directory entry (11)
+ *	form src must point to the first non-separator character.
+ *	dest must point to a 12-byte buffer.
+ */
+uint8_t *fat_canonical_to_dir(uint8_t *dest, uint8_t *src) {
+
+	memset(dest, (int)' ', MSDOS_NAME);
+	dest[11] = 0;
+
+	for (int i = 0; i <= 11; i++) {
+		if (!*src) {
+			break;
+		}
+		if (*src == '/') {
+			break;
+		}
+		if (*src == '.') {
+			i = 7;
+			src++;
+			continue;
+		}
+		*(dest + i) = *src;
+		src++;
+	}
+
+	return dest;
+}
+
+/*
+ *	Convert a filename element from directory entry (11) to canonical (8.3)
+ */
+uint8_t *fat_dir_to_canonical(uint8_t *dest,
+		uint8_t *src, uint8_t dir) {
+        int i;
+        uint8_t *dst;
+
+        dst = dest;
+        memset(dest, 0, MSDOS_NAME + 2);
+        for (i = 0; i < 8; i++) {
+			if (*src != ' ') {
+				*dest = *src;
+				if ((0 == dir) && (*dest >= 'A' && *dest <='Z')) {
+					*dest = (*dest - 'A') + 'a';
+				}
+				dest++;
+			}
+			src++;
+        }
+        if ((*src != ' ') && (0 == dir)) {
+        	*dest++ = '.';
+        }
+        for (i = 0; i < 3; i++) {
+			if (*src != ' ') {
+				*dest = *src;
+				if ((0 == dir) && (*dest >= 'A' && *dest <='Z')) {
+					*dest = (*dest - 'A') + 'a';
+				}
+				dest++;
+			}
+			src++;
+        }
+        return dst;
+}
+
+void get_filename(char *tmppath, char *filename) {
+	char *p;
+
+	p = tmppath;
+	/* strip leading path separators */
+	while (*tmppath == DIR_SEPARATOR){
+		strcpy((char *) tmppath, (char *) tmppath + 1);
+	}
+	while (*(p++));
+	p--;
+	while (p > tmppath && *p != DIR_SEPARATOR) {
+		p--;
+	}
+
+	if (*p == DIR_SEPARATOR) {
+		p++;
+	}
+	fat_canonical_to_dir((uint8_t *) filename, (uint8_t *) p);
+	if (p > tmppath) {
+		p--;
+	}
+	if (*p == DIR_SEPARATOR || p == tmppath) {
+		*p = 0;
+	}
+}
+
+void set_filetime(dir_ent_t *de) {
+	/* TODO set normal time */
+		de->crttime_l = 0x20;	/* 01:01:00am, Jan 1, 2006. */
+		de->crttime_h = 0x08;
+		de->crtdate_l = 0x11;
+		de->crtdate_h = 0x34;
+		de->lstaccdate_l = 0x11;
+		de->lstaccdate_h = 0x34;
+		de->wrttime_l = 0x20;
+		de->wrttime_h = 0x08;
+		de->wrtdate_l = 0x11;
+		de->wrtdate_h = 0x34;
+}
