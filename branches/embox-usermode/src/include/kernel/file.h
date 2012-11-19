a7b510913384330d3e9c30432b5afc3bdeda9dc6
iantonishko <iantonishko@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-11-19 19:15:33 +0000

    Branching embox-usermode

---
+++ branches/embox-usermode/src/include/kernel/file.h
@@ -0,0 +1,35 @@
+/**
+ * @file
+ * @brief Kernel file abstraction
+ *
+ * @date 06.09.11
+ * @author Anton Kozlov
+ */
+
+#ifndef KERNEL_FILE_H_
+#define KERNEL_FILE_H_
+
+#include <types.h>
+#include <stdarg.h>
+
+struct file_desc;
+
+typedef void  *(*FILEOP_OPEN)(struct file_desc *desc,  const char *mode);
+typedef int    (*FILEOP_CLOSE)(struct file_desc *desc);
+typedef size_t (*FILEOP_READ)(void *buf, size_t size, size_t count, void *file);
+typedef size_t (*FILEOP_WRITE)(const void *buf, size_t size, size_t count, void *file);
+typedef int    (*FILEOP_FSEEK)(void *file, long offset, int whence);
+typedef int    (*FILEOP_IOCTL)(void *file, int request, va_list args);
+typedef int    (*FILEOP_FSTAT)(void *file, void *buff);
+
+typedef struct file_operations {
+	FILEOP_OPEN  fopen;
+	FILEOP_CLOSE fclose;
+	FILEOP_READ  fread;
+	FILEOP_WRITE fwrite;
+	FILEOP_FSEEK fseek;
+	FILEOP_IOCTL ioctl;
+	FILEOP_FSTAT fstat;
+} file_operations_t;
+
+#endif /* KERNEL_FILE_H_ */
---
+++ branches/embox-usermode/src/tests/net/parser/request_parser_test.c
@@ -0,0 +1,48 @@
+/**
+ * @file
+ *
+ * @brief
+ *
+ * @date 25.10.2012
+ * @author Vita Loginova
+ */
+
+#include <embox/test.h>
+#include <net/util/request_parser.h>
+#include <lib/url_parser.h>
+#include <stdlib.h>
+
+EMBOX_TEST_SUITE("network/parser/request_parser test");
+
+TEST_CASE("Parse first string") {
+	char *test_get = "GET http://wsdf-365.ru/first_folder/second/index.html#frag01 HTTP/1.1";
+	char *test_get2 = "HEAD /first_folder/second/index.php?p1+p2&p3=54; HTTP/1.1\r\nHost: 10.10.111.1:80";
+	char *test_get3 = "head /first_folder/second/index.php?p1+p2&p3=54; HTTP/1.1\r\nHost: 10.10.111.1:80";
+	char *test_get4 = "HEAD  HTTP/1.1\nHost: 10.10.111.1:80";
+	http_request *parsed_request;
+
+	parsed_request = parse_http(test_get);
+	test_assert_str_equal(parsed_request->method, "GET");
+	test_assert_str_equal(parsed_request->proto, "HTTP/1.1");
+	test_assert_str_equal(parsed_request->parsed_url->host, "wsdf-365.ru");
+	test_assert_str_equal(parsed_request->parsed_url->path, "first_folder/second/index.html");
+	test_assert_str_equal(parsed_request->parsed_url->fragment, "frag01");
+	free_http_request(parsed_request);
+
+	parsed_request = parse_http(test_get2);
+	test_assert_str_equal(parsed_request->method, "HEAD");
+	test_assert_str_equal(parsed_request->proto, "HTTP/1.1");
+	test_assert_str_equal(parsed_request->parsed_url->host, "10.10.111.1");
+	test_assert_str_equal(parsed_request->parsed_url->path, "first_folder/second/index.php");
+	test_assert_str_equal(parsed_request->parsed_url->query, "p1+p2&p3=54;");
+	test_assert_str_equal(parsed_request->parsed_url->port, "80");
+	free_http_request(parsed_request);
+
+	parsed_request = parse_http(test_get3);
+	test_assert_null(parsed_request);
+	free_http_request(parsed_request);
+
+	parsed_request = parse_http(test_get4);
+	test_assert_null(parsed_request);
+	free_http_request(parsed_request);
+}
