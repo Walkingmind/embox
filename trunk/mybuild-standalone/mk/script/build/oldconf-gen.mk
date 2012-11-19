bab944de576ab0207de0c8810be8f77a4ec064e1
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-11-19 11:48:04 +0000

    remove embox-specific build steps (initfs, symbol genereation, etc.) hello world application now seems to work

--- trunk/mybuild-standalone/mk/script/build/oldconf-gen.mk
+++ trunk/mybuild-standalone/mk/script/build/oldconf-gen.mk
@@ -6,56 +6,15 @@
 
 include mk/script/script-common.mk
 
-HOSTCC  = gcc
-HOSTCPP = $(HOSTCC) -E
-
-HOSTCC_VERSION := \
-	$(word 3,$(shell $(HOSTCC) -v 2>&1 | grep -e "^gcc"))
-HOSTCC_VERSION_MAJOR := $(word 1,$(subst ., ,$(HOSTCC_VERSION)))
-
-ifeq ($(HOSTCC_VERSION_MAJOR),4)
-HOSTCC_CPPFLAGS := -iquote $(CONF_DIR)
-else
-HOSTCC_CPPFLAGS := -I $(CONF_DIR) -I-
-endif
-
 build_conf   := $(CONF_DIR)/build.conf
-options_conf := $(CONF_DIR)/options.conf
-lds_conf     := $(CONF_DIR)/lds.conf
-
 build_mk     := $(MKGEN_DIR)/build.mk
-config_h     := $(SRCGEN_DIR)/config.h
-config_lds_h := $(SRCGEN_DIR)/config.lds.h
 
-all : $(build_mk) $(config_h) $(config_lds_h)
+all : $(build_mk)
 
-$(build_mk)     : $(build_conf)
-$(config_h)     : $(options_conf)
-$(config_lds_h) : $(lds_conf)
-
-$(build_mk)     : DEFS := __BUILD_MK__
-$(config_h)     : DEFS := __CONFIG_H__
-$(config_lds_h) : DEFS := __CONFIG_LDS_H__
-
-$(build_mk) :
-	@$(call cmd_notouch_stdout,$@, \
-		$(HOSTCPP) -P -undef -nostdinc $(HOSTCC_CPPFLAGS) $(DEFS:%=-D%) \
-		-MMD -MP -MT $@ -MF $@.d mk/confmacro.S \
-			| sed 's/$$N/\n/g')
-
-$(config_h) $(config_lds_h) :
-	@$(call cmd_notouch_stdout,$@, \
-		$(HOSTCPP) -P -undef -nostdinc $(HOSTCC_CPPFLAGS) $(DEFS:%=-D%) \
-		-MMD -MT $@ -MF $@.d mk/confmacro.S \
-			| sed -e 's/$$N/\n/g' -e 's/$$define/#define/g'; \
-	echo '#define CONFIG_ROOTFS_IMAGE "$(ROOTFS_IMAGE)"') # XXX =/
-
-$(AUTOCONF_DIR)/start_script.inc: $(CONF_DIR)/start_script.inc
+$(build_mk) : $(build_conf)
 	@$(call cmd_notouch_stdout,$@,cat $<)
 
--include $(addsuffix .d,$(build_mk) $(config_h) $(config_lds_h))
-
-# XXX copy-patse
+# XXX copy-paste
 cmd_notouch = \
 	set_on_error_trap() { trap "$$1" INT QUIT TERM HUP EXIT; };            \
 	COMMAND=$(call sh_quote,set_on_error_trap "$(RM) $$OUTFILE"; { $2; }); \
