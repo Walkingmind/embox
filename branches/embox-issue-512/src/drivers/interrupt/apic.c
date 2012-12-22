e5be6ca6a745fa33a3acff5cb101a495444ffdd8
ivan.v.tretyakov@gmail.com <ivan.v.tretyakov@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2012-12-22 19:39:06 +0000

    Branching from trunk to work under issue-512

---
+++ branches/embox-issue-512/src/drivers/interrupt/apic.c
@@ -0,0 +1,133 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 18.12.2012
+ * @author Anton Bulychev
+ */
+
+#include <embox/unit.h>
+
+#include <string.h>
+#include <unistd.h>
+#include <types.h>
+
+#include <asm/msr.h>
+#include <asm/ap.h>
+
+#include "apic.h"
+
+EMBOX_UNIT_INIT(unit_init);
+
+#define lapic_write_icr1(val)	lapic_write(LAPIC_ICR1, val)
+#define lapic_write_icr2(val)	lapic_write(LAPIC_ICR2, val)
+
+static inline uint32_t lapic_id(void) {
+	return lapic_read(LAPIC_ID) >> 24;
+}
+
+static inline uint32_t lapic_errstatus(void)
+{
+	lapic_write(LAPIC_ESR, 0);
+	return lapic_read(LAPIC_ESR);
+}
+
+void lapic_send_startup_ipi(uint32_t apic_id, uint32_t trampoline) {
+	uint32_t val;
+
+	val = 0;
+	val |= apic_id << 24;
+	lapic_write_icr2(val);
+
+	val = 0;
+	val |= (1 << 14) | (6 << 8);
+	val |= ((trampoline >> 12) & 0xFF);
+	lapic_write_icr1(val);
+}
+
+void lapic_send_init_ipi(uint32_t apic_id) {
+	uint32_t val;
+
+	val = 0;
+	val |= apic_id << 24;
+	lapic_write_icr2(val);
+
+	val = 0;
+	val |= (1 << 14) | (5 << 8);
+	lapic_write_icr1(val);
+}
+
+void startup_ap(void) {
+	while (1) {
+		__asm__ __volatile__ ("hlt");
+	}
+}
+
+static inline void lapic_enable_in_msr(void) {
+	uint32_t msr_hi, msr_lo;
+
+	ia32_msr_read(IA32_APIC_BASE, &msr_hi, &msr_lo);
+	msr_lo |= (1 << IA32_APIC_BASE_ENABLE_BIT);
+	ia32_msr_write(IA32_APIC_BASE, msr_hi, msr_lo);
+}
+
+#define TRAMPOLINE_ADDR 0x20000UL
+
+static inline void init_trampoline(void) {
+	/* Initialize gdt */
+	memcpy(__ap_gdt, gdt, sizeof(gdt));
+	gdt_set_gdtr(&__ap_gdtr, __ap_gdt);
+
+	memcpy((void *) TRAMPOLINE_ADDR, &__ap_trampoline,
+			(uint32_t) &__ap_trampoline_end - (uint32_t) &__ap_trampoline);
+}
+
+static inline void cpu_start(int cpu_id) {
+	lapic_send_init_ipi(cpu_id);
+
+	for (int i = 0; i < 20000; i++) {
+		__asm__ __volatile__ ("nop");
+	}
+
+	lapic_send_init_ipi(cpu_id);
+
+	for (int i = 0; i < 20000; i++) {
+		__asm__ __volatile__ ("nop");
+	}
+
+	lapic_send_startup_ipi(cpu_id, TRAMPOLINE_ADDR);
+}
+
+void lapic_enable(void)
+{
+	uint32_t val;
+
+	lapic_enable_in_msr();
+
+	/* Clear error state register */
+	lapic_errstatus();
+
+    /* Set the spurious interrupt vector register */
+	val = lapic_read(LAPIC_SIVR);
+	val |= 0x100;
+	lapic_write(LAPIC_SIVR, val);
+
+	/* Initialize trampoline for the APs */
+	init_trampoline();
+
+    for (int i = 0; i <= 1; i++) {
+    	if (i == lapic_id()) {
+    		continue;
+    	}
+
+    	cpu_start(i);
+    }
+
+    usleep(5000);
+}
+
+static int unit_init(void) {
+	lapic_enable();
+
+	return 0;
+}
---
+++ branches/embox-issue-512/src/kernel/time/timer/strategy/head_timer.c
@@ -0,0 +1,94 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 22.07.10
+ * @author Andrey Baboshin
+ * @author Ilia Vaprol
+ * @author Fedor Burdun
+ */
+
+#include <kernel/time/timer.h>
+
+
+static DLIST_DEFINE(sys_timers_list); /* list head to timers */
+
+void timer_strat_start(struct sys_timer *tmr) {
+	struct sys_timer *it_tmr, *tmp;
+
+	dlist_head_init(&tmr->lnk);
+	timer_set_started(tmr);
+
+	tmr->cnt = tmr->load;
+
+	/* find first element that its time bigger than inserting @new_time */
+	dlist_foreach_entry(it_tmr, tmp, &sys_timers_list,lnk) {
+		if (it_tmr->cnt >= tmr->cnt) {
+			/* decrease value of next timer after inserting */
+			it_tmr->cnt -= tmr->cnt;
+
+			dlist_add_prev(&tmr->lnk, &it_tmr->lnk);
+
+			return;
+		}
+		tmr->cnt -= it_tmr->cnt;
+
+	}
+
+	/* add the latest timer to end of list */
+	dlist_add_prev(&tmr->lnk, &sys_timers_list);
+}
+
+void timer_strat_stop(struct sys_timer *ptimer) {
+	struct sys_timer *next_tmr;
+
+	timer_set_stopped(ptimer);
+
+	if(ptimer->lnk.next != &sys_timers_list) {
+		next_tmr = (struct sys_timer *)ptimer->lnk.next;
+		next_tmr->cnt += ptimer->cnt;
+	}
+
+	dlist_del(&ptimer->lnk);
+}
+
+static inline bool timers_need_schedule(void) {
+	if(dlist_empty(&sys_timers_list)) {
+		return false;
+	}
+
+	if(0 == --((sys_timer_t*)sys_timers_list.next)->cnt) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+static inline void timers_schedule(void) {
+	struct sys_timer *timer,*tmp;
+	uint32_t nxt_cnt = 0; /* we schedule first timer */
+
+	dlist_foreach_entry(timer, tmp, &sys_timers_list, lnk) {
+		nxt_cnt = tmp->cnt;
+		timer->handle(timer, timer->param);
+
+		timer_strat_stop(timer);
+		if(timer_is_periodic(timer)) {
+			timer_strat_start(timer);
+		}
+		if (0 != nxt_cnt) {
+			return;
+		}
+	}
+}
+
+/**
+ * For each timer in the timers array do the following: if the timer is enable
+ * and the counter of this timer is the zero then its initial value is assigned
+ * to the counter and the function is executed.
+ */
+void timer_strat_sched(void) {
+	if(timers_need_schedule()) {
+		timers_schedule();
+	}
+}
