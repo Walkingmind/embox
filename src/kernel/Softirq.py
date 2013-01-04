958913c648587b3d0af5e6a4287334c83a15fe0e
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-01-04 18:50:00 +0000

    [pybuild] some unneeded .notready removed, src/framework done

--- src/kernel/Softirq.py
+++ src/kernel/Softirq.py
@@ -4,7 +4,12 @@ package('embox.kernel')
 module(
     'softirq',
     sources = ['softirq.c'],
+
     options = [
 	Integer('nrs_total', default = 32)
-    ]
+    ],
+
+    depends = [
+        NoRuntime('critical'),
+    ],
 )
