7f96c1cdefa87f8bb62cbf1bb7afcc897917db9f
iantonishko <iantonishko@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-02-08 18:21:44 +0000

    [embox-smp] percpu fix.

--- branches/embox-smp/src/kernel/smp/percpu.c
+++ branches/embox-smp/src/kernel/smp/percpu.c
@@ -11,8 +11,13 @@
 
 #include <kernel/percpu.h>
 
+/* TODO: Delete it */
+#ifdef SMP
+
 void percpu_init(void) {
 	for (unsigned int i = 1; i < NCPU; i++) {
 		memcpy(__PERCPU_START + i*__PERCPU_LEN, __PERCPU_START, __PERCPU_LEN);
 	}
 }
+
+#endif /* SMP */
