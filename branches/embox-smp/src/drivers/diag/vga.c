1fcd9a4d8981b6f8110a16aa468113906ea8f5da
iantonishko <iantonishko@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-02-08 08:38:20 +0000

    [embox-smp] merge 8469:8498 from trunk.

--- branches/embox-smp/src/drivers/diag/vga.c
+++ branches/embox-smp/src/drivers/diag/vga.c
@@ -10,26 +10,19 @@
 #include <drivers/keyboard.h>
 #include <drivers/vga_console.h>
 
-static char diag_vga_getc(void) {
+char diag_getc(void) {
 	return keyboard_getc();
 }
 
-static void diag_vga_putc(char ch) {
+void diag_putc(char ch) {
 	vga_putc(vga_console_diag(), ch);
 }
 
-static int diag_vga_kbhit(void) {
+int diag_kbhit(void) {
 	return keyboard_has_symbol();
 }
 
-static const struct diag_ops diag_vga_ops = {
-	.getc = &diag_vga_getc,
-	.putc = &diag_vga_putc,
-	.kbhit = &diag_vga_kbhit
-};
-
 void diag_init(void) {
-	diag_common_set_ops(&diag_vga_ops);
 	vga_console_init(vga_console_diag(), 80, 25);
 	keyboard_init();
 }
