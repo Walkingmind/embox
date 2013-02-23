f4e54e28e690b491d3e55bf83f52be7b9e68d6ba
ivan.v.tretyakov <ivan.v.tretyakov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-02-23 13:07:31 +0000

    Merge with trunk. Conflicts in kfsop.c, file_op_test.c, kfsop.h and fsop.c resolved manually

--- branches/embox-issue-512/src/include/fs/xattr.h
+++ branches/embox-issue-512/src/include/fs/xattr.h
@@ -1,8 +1,8 @@
 /**
- * @file 
- * @brief 
+ * @file
+ * @brief
  *
- * @author  Anton Kozlov 
+ * @author  Anton Kozlov
  * @date    30.01.2013
  */
 
@@ -10,7 +10,7 @@
 #define FS_XATTR_H_
 
 #include <fs/node.h>
-#include <fs/fs_drv.h>
+#include <fs/fs_driver.h>
 
 #define XATTR_CREATE  (1 << 0)
 #define XATTR_REMOVE  (1 << 1)
@@ -18,10 +18,10 @@
 
 #define XATTR_MAX_VSIZE  0xFF
 
-extern int kfile_xattr_get(struct node *node, const char *name, char *value, 
+extern int kfile_xattr_get(struct node *node, const char *name, char *value,
 		size_t len);
 
-extern int kfile_xattr_set(struct node *node, const char *name, 
+extern int kfile_xattr_set(struct node *node, const char *name,
 		const char *value, size_t len, int flags);
 
 extern int kfile_xattr_list(struct node *node, char *list, size_t len);
