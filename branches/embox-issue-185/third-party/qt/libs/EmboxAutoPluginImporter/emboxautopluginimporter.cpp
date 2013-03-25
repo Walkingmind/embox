9123cb58920f352ca2297941d8d6b0189a1a8e98
ivan.v.tretyakov <ivan.v.tretyakov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-03-25 20:55:58 +0000

    This branched is merged to master and not needed anymore

--- branches/embox-issue-185/third-party/qt/libs/EmboxAutoPluginImporter/emboxautopluginimporter.cpp
+++
@@ -1,8 +0,0 @@
-#include <QtPlugin>
-
-Q_IMPORT_PLUGIN(vnc)
-
-extern "C" __attribute__((naked))
-void embox_auto_plugin_importer_linker_hook(void) {
-	// Just want this compilation module to link in
-}
