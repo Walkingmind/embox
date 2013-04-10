801a268b6cbf650bbad7a24c3e0e689b2256d815
Anton.Bondarev2310@gmail.com <Anton.Bondarev2310@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-04-10 09:34:15 +0000

    [pci] PLX ID

--- branches/pci/src/include/drivers/pci/pci_id.h
+++ branches/pci/src/include/drivers/pci/pci_id.h
@@ -17,6 +17,7 @@
 #define PCI_VENDOR_ID_RDC                 0x17f3
 #define PCI_VENDOR_ID_INTEL               0x8086
 #define PCI_VENDOR_ID_BOCHS               0x1234
+#define PCI_VENDOR_ID_PLX                 0x10B5
 
 /* Cirrus device id's */
 #define PCI_DEV_ID_CIRRUS_5446            0x00b8
@@ -65,5 +66,9 @@
 
 /* Bochs device id's */
 #define PCI_DEV_ID_BOCHS_VGA              0x1111
+
+
+#define PCI_DEV_ID_PLX_PEX8619            0x8619
+
 
 #endif /* PCI_ID_H_ */
