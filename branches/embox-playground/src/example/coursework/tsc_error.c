09474586fa65e61a093991b74bc897caa4fc142d
iantonishko <iantonishko@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-05-06 10:16:10 +0000

    [embox-playground]

--- branches/embox-playground/src/example/coursework/tsc_error.c
+++ branches/embox-playground/src/example/coursework/tsc_error.c
@@ -19,7 +19,7 @@
 
 EMBOX_EXAMPLE(run);
 
-#define COUNT 10
+#define COUNT 500
 #define CYCLE 100000000
 
 static unsigned long long f, s, d;
@@ -36,26 +36,77 @@ static inline unsigned int get_cr0(void) {
 
 static inline unsigned long long rdtsc(void) {
 	unsigned hi, lo;
-	__asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+	__asm__ __volatile__(
+			"cpuid\n\t"
+			"rdtsc"
+		: "=a"(lo), "=d"(hi)
+		: "a"(0)
+//		: "%ebx", "%ecx"
+	);
 	return ((unsigned long long) lo) | (((unsigned long long) hi) << 32);
 }
 
+static inline void cycle(int top) {
+	__asm__ __volatile__ (
+			"movl $0, %%eax\n\t"
+			"cc:\n\t"
+			"inc %%eax\n\t"
+			"cmp %%ebx, %%eax\n\t"
+			"jl cc"
+		:
+		: "b"(top)
+		: "%eax"
+	);
+}
+
+#define DIFF_VAL 20
+
+unsigned long long val[DIFF_VAL];
+int count[DIFF_VAL];
+
 static int run(int argc, char **argv) {
 	ipl_t ipl;
+	int i, j;
 
 //	set_cr0(get_cr0() | (1UL << 30)); /* Disable caching */
 
-	for (int i = 1; ; i++) {
+	for (i = 1; i <= COUNT; i++) {
+
 		ipl = ipl_save();
 		{
 			f = rdtsc();
-			for (volatile int j = 0; j < CYCLE; j++) ;
+			//for (volatile int j = 0; j < CYCLE; j++) ;
+			cycle(CYCLE);
 			s = rdtsc();
 		}
 		ipl_restore(ipl);
 
 		d = s - f;
-		printf("CYCLE #%5d: %9lld\n", i, d);
+		//printf("CYCLE #%5d: %9lld\n", i, d);
+
+		for (j = 0; j < DIFF_VAL; j++) {
+			if (val[j] == d) {
+				count[j]++;
+				break;
+			}
+
+			if (count[j] == 0) {
+				val[j] = d;
+				count[j] = 1;
+				break;
+			}
+		}
+
+		if (j == DIFF_VAL) {
+			printf("Too many different values!\n");
+			break;
+		}
+	}
+
+	printf("\nSummary (%d iterations):\n", i - 1);
+	for (j = 0; j < DIFF_VAL; j++) {
+		if (count[j] == 0) break;
+		printf("VAL: %9lld, COUNT: %d\n", val[j], count[j]);
 	}
 
 	return ENOERR;
