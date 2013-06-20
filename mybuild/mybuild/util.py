2b4dccbcd82d2343a3ebe52db6b1e8bcabe89b09
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-06-20 17:37:32 +0000

    [pybuild] merge (reintegrate) mybuild-experimental branch (r9806-r9869)

--- mybuild/mybuild/util.py
+++ mybuild/mybuild/util.py
@@ -2,9 +2,77 @@
 Misc stuff. --Eldar
 """
 
+from collections import namedtuple
+from collections import Mapping
+
+from compat import *
+
+
+class Pair(namedtuple('_Pair', 'false true')):
+    __slots__ = ()
+
+    def _mapwith(self, func):
+        return Pair._make(map(func, self))
+
+bools = Pair(False, True)
+
+def to_dict(iterable_or_mapping, check_exclusive=False):
+    if isinstance(iterable_or_mapping, dict):
+        return iterable_or_mapping
+
+    if not check_exclusive or isinstance(iterable_or_mapping, Mapping):
+        return dict(iterable_or_mapping)
+
+    items = list(iterable_or_mapping)
+    ret_dict = dict(items)
+    if len(ret_dict) != len(items):
+        raise ValueError('Item(s) with conflicting keys detected')
+
+    return ret_dict
+
+
+if hasattr(0, 'bit_length'):
+
+    def single_set_bit(x):
+        if x > 0 and not (x & (x-1)):
+            return x.bit_length() - 1
+
+else:
+    def single_set_bit(x):
+        if x > 0 and not (x & (x-1)):
+            return len(bin(x)) - 3  # 5 -> bin=0b101 -> len=5 -> ret=2
+
+
 def singleton(cls):
     """Decorator for declaring and instantiating a class in-place."""
     return cls()
+
+
+def pop_iter(s, pop=None, pop_meth='pop'):
+    get_next = pop if pop is not None else getattr(s, pop_meth)
+    while s:
+        yield get_next()
+
+def send_next_iter(it, first=None):
+    get_next = iter(it).next
+    received = first
+    while True:
+        received = (yield received if received is not None else get_next())
+        if received is not None:
+            yield  # from send
+
+
+def until_fixed(func):
+    prev = func()
+    yield prev
+
+    next = func()
+
+    while prev != next:
+        yield next
+
+        prev = next
+        next = func()
 
 def unique(iterable, key=id):
     """
@@ -59,11 +127,11 @@ class NotifyingMixin(object):
         self.__subscribers = []
 
     def _notify(self, *args, **kwargs):
-        for fxn in self.__subscribers:
-            fxn(*args, **kwargs)
+        for func in self.__subscribers:
+            func(*args, **kwargs)
 
-    def subscribe(self, fxn):
-        self.__subscribers.append(fxn)
+    def subscribe(self, func):
+        self.__subscribers.append(func)
 
 
 class InstanceBoundTypeMixin(object):
