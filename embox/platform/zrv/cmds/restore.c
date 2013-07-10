ca127f49c3ddf4c386f1f3a75d297beba603a0dd
pavel.cherstvov@gmail.com <pavel.cherstvov@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-07-10 09:08:56 +0000

    -f option (files overwrite) added to restore

--- embox/platform/zrv/cmds/restore.c
+++ embox/platform/zrv/cmds/restore.c
@@ -9,27 +9,77 @@
 #include <stdio.h>
 #include <cmd/shell.h>
 #include <embox/cmd.h>
+#include <unistd.h>
+#include <string.h>
+
 
 EMBOX_CMD(restore_cmd);
 
 static void usage(char *cmd) {
-	printf("Usage: %s DIR BACKUP", cmd);
+	printf("Usage: %s BACKUP DIR [-f]\n", cmd);
 }
 
 #define BUFLEN 160
+#define MIN_ARGS_OF_RESTORE 3
 
 static int restore_cmd(int argc, char *argv[]) {
 	const struct shell *sh = shell_any();
 	char buf[BUFLEN];
+	char *src = argv[1];
+	char *dst = argv[2];
+	int opt;
+	int min_argc = MIN_ARGS_OF_RESTORE;
 
-	if (argc != 3) {
-		usage(argv[0]);
+	getopt_init();
+
+	while (-1 != (opt = getopt(argc, argv, "hf"))) {
+
+		switch (opt) {
+		case '?':
+		case 'h': /* show help */
+			usage(argv[0]);
+			return ENOERR;
+		case 'f': /* force overwrite */
+			min_argc = 4;
+
+			if (argc != min_argc) {
+				usage(argv[0]);
+				return -EINVAL;
+			}
+
+			if(0 != strncmp(dst, "/", 1) ||
+			   0 != strncmp(src, "/", 1)) {
+				return -EINVAL;
+			}
+
+			if (-1 == remove(dst)) {
+				return -errno;
+			}
+			break;
+
+			if (-1 == mkdir(dst, 0)) {
+				return -errno;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+
+	if (argc != min_argc) {
+			usage(argv[0]);
+			return -EINVAL;
+		}
+
+	if(0 != strncmp(dst, "/", 1) ||
+	   0 != strncmp(src, "/", 1)) {
 		return -EINVAL;
 	}
 
-	if (BUFLEN <= snprintf(buf, BUFLEN, "backup %s %s", argv[1], argv[2])) {
+	if (BUFLEN <= snprintf(buf, BUFLEN, "backup %s %s", src, dst)) {
 		return -ENOMEM;
 	}
 
 	return shell_exec(sh, buf);
+
 }
