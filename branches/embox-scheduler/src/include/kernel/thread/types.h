c5a20839a0cdf7074627f08247afafac1c12a6b1
anton.bondarev2310@gmail.com <anton.bondarev2310@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-08-06 14:39:37 +0000

    [sched] clean thread/state.h a little

--- branches/embox-scheduler/src/include/kernel/thread/types.h
+++ branches/embox-scheduler/src/include/kernel/thread/types.h
@@ -1,6 +1,7 @@
 /**
  * @file
- * @brief TODO documentation for types.h -- Alina
+ *
+ * @brief Describes thread descriptor structure
  *
  * @date 23.03.11
  * @author Alina Kramar
@@ -9,25 +10,21 @@
 #ifndef KERNEL_THREAD_TYPES_H_
 #define KERNEL_THREAD_TYPES_H_
 
-#include <stddef.h>
-#include <sys/types.h>
-
-
-typedef int __thread_id_t;
-typedef unsigned int __thread_state_t;
+#include <stddef.h> /* only for stack size */
 
 #include <hal/context.h>
 
+#include <kernel/thread/state.h>
 #include <kernel/sched/sched_strategy.h>
 #include <util/dlist.h>
-#include <kernel/thread/thread_priority.h>
 #include <kernel/thread/wait_data.h>
 
+typedef int __thread_id_t;
 
-struct runq;
+struct task;
 
 struct thread {
-	__thread_state_t   state;         /**< Current state. */
+	thread_state_t   state;           /**< Current state. */
 
 	struct context     context;       /**< Architecture-dependent CPU state. */
 
@@ -37,18 +34,20 @@ struct thread {
 		void          *run_ret;       /**< Return value of the routine. */
 		void          *join_ret;      /**< Exit value of a join target. */
 	} /* unnamed */;
+	/* TODO extract work with stack to separate module */
 	void              *stack;         /**< Allocated thread stack buffer. */
 	size_t             stack_sz;      /**< Stack size. */
 
 	__thread_id_t      id;            /**< Unique identifier. */
 
 	struct task       *task;          /**< Task belong to. */
-	struct dlist_head  thread_link;   /**<list's link holding task threads. */
+	struct dlist_head  thread_link;   /**< list's link holding task threads. */
 
 	struct thread     *joined;        /**< Thread which joined to this. */
 
-	struct wait_data   wait_data;
+	struct wait_data   wait_data;     /**< Hold data for waiting mode */
 
+	/* TODO may be named 'struct sched_attributes' */
 	struct sched_strategy_data sched_priv; /**< Scheduler-private data. */
 };
 
