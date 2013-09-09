befb2517e08b0664e7d7704aed2fc19b40796b68
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-09-09 16:33:48 +0000

    [pybuild-example] working hello world example

--- pybuild-example/src/main.c
+++ pybuild-example/src/main.c
@@ -4,15 +4,10 @@
  * @date Jun 25, 2013
  * @author: Anton Bondarev
  */
+
 #include <stdio.h>
 
 int main(void) {
-	int i;
-
-	i = OPTION_GET(NUMBER, iteration);
-	for(; i > 0; i--) {
-		printf("hello mybuild v 0.5");
-	}
-
+	printf("Hello from Mybuild v%s!\n", MYBUILD_VERSION);
 	return 0;
 }
