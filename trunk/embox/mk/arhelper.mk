1ba6b57eefbecdf64b3ccc9c4a60f2faff366409
Eldar.Abusalimov <Eldar.Abusalimov@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-09-29 20:25:34 +0000

    [appmods] use objcopy to rename app sections insead of ld

--- trunk/embox/mk/arhelper.mk
+++ trunk/embox/mk/arhelper.mk
@@ -9,9 +9,9 @@
 #   TARGET: name of the output archive
 #   A_FILES: input archives
 #   O_FILES: input objects
-#   RELOC:  a flag indicating whether to pass objects through LD or not
+#   APP_ID:  an identifier which (if set) is used to rename object sections
 #   AR and ARFLAGS: archiver ('ar') and creation flags (e.g. 'rcs')
-#   LD and LDFLAGS: linker (used when RELOC is set)
+#   OBJCOPY and OBJCOPYFLAGS: used when APP_ID is set
 #
 
 $(TARGET) :  # default
@@ -26,7 +26,7 @@ override O_FILES := $(sort $(call normalize_path,$(O_FILES)))
 override A_FILES := $(sort $(call normalize_path,$(A_FILES)))
 
 
-ifdef RELOC
+ifdef APP_ID
 
 define o_template
 $(obj) : $o
@@ -40,9 +40,9 @@ ifneq ($(words $(O_FILES)),$(words $(sort $(notdir $(o_objs)))))
 $(error Can't make an archive of objects with the same file name)
 endif
 
-else  # no RELOC
+else  # no APP_ID
 o_objs = $(O_FILES)
-endif # RELOC
+endif # APP_ID
 
 
 $(A_FILES:%=%.x) : %.x : %
@@ -53,12 +53,12 @@ define a_template
 members-$(abs) := $(shell $(AR) t '$a')  # x.o
 $(members-$(abs):%=$a.x/%) : $a.x
 
-ifdef RELOC
+ifdef APP_ID
 objects-$(abs) := $(members-$(abs):%=$a.%)  # path/to/lib.a.x.o
 $(objects-$(abs)) : $a.% : $a.x/%
-else  # no RELOC
+else  # no APP_ID
 objects-$(abs) := $(members-$(abs):%=$a.x/%)  # path/to/lib.a.x/x.o
-endif # RELOC
+endif # APP_ID
 
 endef
 
@@ -68,10 +68,9 @@ a_objs := $(foreach a,$(A_FILES),$(foreach abs,$(abspath $a), \
 
 objs = $(o_objs) $(a_objs)
 
-ifdef RELOC
+ifdef APP_ID
 $(objs) :
-	$(LD) -r $(LDFLAGS) $< \
-		-o $@
+	$(OBJCOPY) $(OBJCOPYFLAGS) $< $@
 endif
 
 
