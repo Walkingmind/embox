3bdde6519501963132d7e4155c62f3d11d31df4f
anton.bondarev2310@gmail.com <anton.bondarev2310@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2013-10-14 12:39:14 +0000

    disassembly on demands (use "DISASSEMBLY=y" in build.conf)

--- trunk/embox/mk/main.mk
+++ trunk/embox/mk/main.mk
@@ -232,6 +232,19 @@ Usage: $(MAKE) xconfig
 endef # xconfig
 
 #
+# Disassembly
+#
+
+.PHONY : disasm
+disasm :
+	+@$(make_mybuild) build DISASSEMBLY=y
+
+define help-disasm
+Usage: $(MAKE) disasm
+  Disassembly image file
+endef # disasm
+
+#
 # Cleaning targets.
 #
 
@@ -343,7 +356,8 @@ help_entries := \
 	clean \
 	confclean \
 	cacheclean \
-	distclean
+	distclean \
+	disasm
 help_targets := $(help_entries:%=help-%)
 
 # Fixup documentation variables escaping colons (':' -> '$$:').
