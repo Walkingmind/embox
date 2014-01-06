107179a14dc2b3dad1154be293919f7a2ecbd93b
anton.bondarev2310@gmail.com <anton.bondarev2310@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-01-06 12:35:26 +0000

    remove branch embox_byte_order

--- branches/embox_byte_order/src/arch/arm/include/asm/hal/arch.h
+++
@@ -1,9 +0,0 @@
-#ifndef HAL_ARCH_H_
-# error "Do not include this file directly!"
-#endif /* HAL_ARCH_H_ */
-
-#ifndef __BYTE_ORDER__
-# error "You need define byte order for arm architecture"
-#endif
-
-#define __PLATFORM_ARCH "arm"
