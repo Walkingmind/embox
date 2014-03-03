f9cf4294cd45a945414268ce73fae5ea4ee1f38e
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-03-03 15:49:44 +0000

    mk oops r12878 command made loud

--- trunk/embox/mk/main.mk
+++ trunk/embox/mk/main.mk
@@ -154,7 +154,7 @@ endif # templates
 .PHONY : $(templates:%=confload-%)
 $(templates:%=confload-%) : confload-% : confclean
 	@$(MKDIR) $(CONF_DIR)
-	$(CP) -fR $(call template_name2dir,$*)/* $(CONF_DIR)
+	@$(CP) -fR $(call template_name2dir,$*)/* $(CONF_DIR)
 	@echo 'Config complete'
 
 define help-confload
