bb613f09cbf0cdb87d2eba21fc64c5c7ac0efa76
ki.stfu@gmail.com <ki.stfu@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-04-02 12:04:08 +0000

    [crosstool] update crosstool; remove old patches

--- crosstool/crosstool.sh
+++ crosstool/crosstool.sh
@@ -36,18 +36,20 @@ PATCHES="$(ls $PATCHES_DIR/*.patch 2>/dev/null)
 
 declare -A GET_URL
 # Keys:
-#  0 - binutils
-#  1 - gmp
+#  0 - gmp
+#  1 - mpfr
 #  2 - mpc
-#  3 - mpfr
+#  3 - binutils
 #  4 - gcc
 #  5 - gdb
-GET_URL[0]="http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/binutils/binutils-2.22.tar.gz"
-GET_URL[1]="http://mirrors.kernel.org/gnu/gmp/gmp-5.0.2.tar.bz2"
-GET_URL[2]="http://www.multiprecision.org/mpc/download/mpc-0.9.tar.gz"
-GET_URL[3]="http://www.mpfr.org/mpfr-current/mpfr-3.1.2.tar.bz2"
-GET_URL[4]="http://www.mirrorservice.org/sites/ftp.gnu.org/gnu/gcc/gcc-4.6.4/gcc-4.6.4.tar.bz2"
-GET_URL[5]="http://ftp.gnu.org/gnu/gdb/gdb-7.6.tar.bz2"
+GET_URL[0]="http://ftp.gnu.org/gnu/gmp/gmp-5.1.3.tar.bz2"
+GET_URL[1]="http://ftp.gnu.org/gnu/mpfr/mpfr-3.1.2.tar.gz"
+GET_URL[2]="http://ftp.gnu.org/gnu/mpc/mpc-1.0.2.tar.gz"
+GET_URL[3]="http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.bz2"
+GET_URL[4]="http://ftp.gnu.org/gnu/gcc/gcc-4.8.2/gcc-4.8.2.tar.bz2"
+GET_URL[5]="http://ftp.gnu.org/gnu/gdb/gdb-7.7.tar.bz2"
+
+DOWNLOAD=../download
 
 for i in $(seq 0 $((${#GET_URL[@]} - 1))); do
 	TARBALL[$i]=$(basename ${GET_URL[$i]})
@@ -55,32 +57,19 @@ for i in $(seq 0 $((${#GET_URL[@]} - 1))); do
 done
 
 do_download() {
-	print_msg "downloading start" 
+	print_msg "Download sources"
 	for i in $(seq 0 $((${#GET_URL[@]} - 1))); do
-		print_msg "Download ${TARBALL[$i]}"
-		if [ ! -e ${TARBALL[$i]} ]; then
-			wget ${GET_URL[$i]}
-		fi
+		print_msg "Downloading ${TARBALL[$i]}"
+		wget -c ${GET_URL[$i]} -P $DOWNLOAD
 	done
-	print_msg "downloading done"
 }
 
 do_unpack() {
-	print_msg "unpack start"
+	print_msg "Extract sources"
 	for i in $(seq 0 $((${#GET_URL[@]} - 1))); do
-		print_msg "Unpack ${TARBALL[$i]}"
-		[ -d ${NAME[$i]} ] || tar xaf ${TARBALL[$i]}
+		print_msg "Extracting ${TARBALL[$i]}"
+		[ -d ${NAME[$i]} ] || tar xaf $DOWNLOAD/${TARBALL[$i]}
 	done
-
-	print_msg "Set symlinks for gcc"
-	ln -s ../${NAME[1]} ${NAME[4]}/gmp
-	ln -s ../${NAME[2]} ${NAME[4]}/mpc
-	ln -s ../${NAME[3]} ${NAME[4]}/mpfr
-
-	print_msg "Set symlinks for gdb"
-	ln -s ../${NAME[1]} ${NAME[5]}/gmp
-	ln -s ../${NAME[2]} ${NAME[5]}/mpc
-	ln -s ../${NAME[3]} ${NAME[5]}/mpfr
 
 	print_msg "Apply patches"
 	for f in $PATCHES; do
@@ -89,65 +78,203 @@ do_unpack() {
 	done
 }
 
-do_binutils() {
-	print_msg "Build Binutils start"
-	mkdir build-binutils 
-	pushd build-binutils > /dev/null
-	../${NAME[0]}/configure \
-		--prefix=$TMP_DIR/$TARGET-${NAME[4]} \
-		--target=$TARGET \
-		--disable-werror \
-		--disable-nls
-	make && make install || error_exit "Building binutils failed"
-
+do_gmp() {
+	local source_dir="../${NAME[0]}"
+	local build_dir="build-gmp"
+	local install_dir="$TMP_DIR/install-gmp"
+	print_msg "Build gmp start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		$source_dir/configure \
+			--prefix=$install_dir \
+			|| error_exit "Configuration gmp failed"
+	fi
+	make -j `nproc` -q all
+	if [ $? -ne 0 ]; then
+		#make -j `nproc` all && make -j `nproc` check && make -j `nproc` install
+		make -j `nproc` all && make -j `nproc` install \
+			|| error_exit "Building gmp failed"
+	fi
 	popd > /dev/null
-	print_msg "Build Binutils done"
+	print_msg "Build gmp done"
+}
+
+do_mpfr() {
+	local source_dir="../${NAME[1]}"
+	local build_dir="build-mpfr"
+	local install_dir="$TMP_DIR/install-mpfr"
+	local gmp_dir="$TMP_DIR/install-gmp"
+	print_msg "Build mpfr start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		$source_dir/configure \
+			--prefix=$install_dir \
+			--with-gmp=$gmp_dir \
+			|| error_exit "Configuration mpfr failed"
+	fi
+	make -j `nproc` -q all
+	if [ $? -ne 0 ]; then
+		make -j `nproc` all && make -j `nproc` install \
+			|| error_exit "Building mpfr failed"
+	fi
+	popd > /dev/null
+	print_msg "Build mpfr done"
+}
+
+do_mpc() {
+	local source_dir="../${NAME[2]}"
+	local build_dir="build-mpc"
+	local install_dir="$TMP_DIR/install-mpc"
+	local gmp_dir="$TMP_DIR/install-gmp"
+	local mpfr_dir="$TMP_DIR/install-mpfr"
+	print_msg "Build mpc start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		$source_dir/configure \
+			--prefix=$install_dir \
+			--with-gmp=$gmp_dir \
+			--with-mpfr=$mpfr_dir \
+			|| error_exit "Configuration mpc failed"
+	fi
+	make -j `nproc` -q all
+	if [ $? -ne 0 ]; then
+		make -j `nproc` all && make -j `nproc` install \
+			|| error_exit "Building mpc failed"
+	fi
+	popd > /dev/null
+	print_msg "Build mpc done"
+}
+
+do_binutils() {
+	local source_dir="../${NAME[3]}"
+	local build_dir="build-binutils"
+	local install_dir="$TMP_DIR/install-binutils"
+	local gmp_dir="$TMP_DIR/install-gmp"
+	local mpfr_dir="$TMP_DIR/install-mpfr"
+	local mpc_dir="$TMP_DIR/install-mpc"
+	print_msg "Build binutils start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		$source_dir/configure \
+			--prefix=$install_dir \
+			--target=$TARGET \
+			--with-float=soft \
+			--enable-soft-float
+			--with-mpfr=$mpfr_dir \
+			--with-gmp=$gmp_dir \
+			--with-mpc=$mpc_dir \
+			|| [ -e Makefile ] || error_exit "Configuration binutils failed"
+	fi
+	make -j `nproc` -q all
+	if [ $? -ne 0 ]; then
+		make -j `nproc` all && make -j `nproc` install \
+			|| error_exit "Building binutils failed"
+	fi
+	popd > /dev/null
+	print_msg "Build binutils done"
 }
 
 do_gcc() {
-	print_msg "Build GCC start"
-	mkdir build-gcc
-	pushd build-gcc > /dev/null
-	../${NAME[4]}/configure \
-		--prefix=$TMP_DIR/$TARGET-${NAME[4]} \
-		--target=$TARGET \
-		--disable-werror \
-		--with-gnu-ld \
-		--disable-nls \
-		--disable-multilib \
-		--with-gnu-as \
-		--without-headers \
-		--enable-languages=c,c++ \
-		--disable-libssp \
-		--with-mpfr-include=$(pwd)/../${NAME[4]}/mpfr/src \
-		--with-mpfr-lib=$(pwd)/mpfr/src/.libs \
-		$TARGET_OPTIONS
-	make && make install || error_exit "Building gcc failed"
+	local source_dir="../${NAME[4]}"
+	local build_dir="build-gcc"
+	local install_dir="$TMP_DIR/install-gcc"
+	local gmp_dir="$TMP_DIR/install-gmp"
+	local mpfr_dir="$TMP_DIR/install-mpfr"
+	local mpc_dir="$TMP_DIR/install-mpc"
+	local binutils_dir="$TMP_DIR/install-binutils"
+	local path=$PATH:$binutils_dir/bin
+	print_msg "Build gcc start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		PATH=$path $source_dir/configure \
+			--prefix=$install_dir \
+			--target=$TARGET \
+			--disable-multilib \
+			--disable-libssp \
+			--without-headers \
+			--without-newlib \
+			--with-gnu-as \
+			--with-gnu-ld \
+			--enable-languages=c,c++ \
+			--enable-soft-float \
+			--with-gmp=$gmp_dir \
+			--with-mpfr=$mpfr_dir \
+			--with-mpc=$mpc_dir \
+			$TARGET_OPTIONS \
+			|| error_exit "Configuration gcc failed"
+	fi
+	make -j `nproc` -q all-gcc all-target-libgcc
+	if [ $? -ne 0 ]; then
+		PATH=$path make -j `nproc` all-gcc all-target-libgcc \
+			&& PATH=$path make -j `nproc` install-gcc install-target-libgcc \
+			|| error_exit "Building gcc failed"
+	fi
 	popd > /dev/null
-	print_msg "Build GCC done"
+	print_msg "Build gcc done"
 }
 
 do_gdb() {
-	print_msg "Build GDB start"
-	mkdir build-gdb 
-	pushd build-gdb > /dev/null
-	../${NAME[5]}/configure \
-		--prefix=$TMP_DIR/$TARGET-${NAME[4]} \
-		--target=$TARGET \
-		--with-mpfr-include=$(pwd)/../${NAME[4]}/mpfr/src \
-		--with-mpfr-lib=$(pwd)/mpfr/src/.libs 
-
-	make && make install || error_exit "Building gdb failed"
+	local source_dir="../${NAME[5]}"
+	local build_dir="build-gdb"
+	local install_dir="$TMP_DIR/install-gdb"
+	local gmp_dir="$TMP_DIR/install-gmp"
+	local mpfr_dir="$TMP_DIR/install-mpfr"
+	local mpc_dir="$TMP_DIR/install-mpc"
+	print_msg "Build gdb start"
+	if [ ! -d $build_dir ]; then
+		mkdir $build_dir
+	fi
+	pushd $build_dir > /dev/null
+	if [ ! -e Makefile ]; then
+		$source_dir/configure \
+			--prefix=$install_dir \
+			--target=$TARGET \
+			--with-gmp=$gmp_dir \
+			--with-mpfr=$mpfr_dir \
+			--with-mpc=$mpc_dir \
+			|| error_exit "Configuration gdb failed"
+	fi
+	make -j `nproc` -q all
+	if [ $? -ne 0 ]; then
+		make -j `nproc` all && make -j `nproc` install \
+			|| error_exit "Building gdb failed"
+	fi
 	popd > /dev/null
-	print_msg "Build GDB done"
+	print_msg "Build gdb done"
 }
 
 makepkg() {
+	local binutils_dir="install-binutils"
+	local gcc_dir="install-gcc"
+	local gdb_dir="install-gdb"
+	local pkg_dir="$TARGET-${NAME[4]}"
+	print_msg "Prepare package directory"
+	if [ -d $pkg_dir ]; then
+		rm -rf $pkg_dir/*
+	else
+		mkdir $pkg_dir
+	fi
+	cp -r $binutils_dir/* $gcc_dir/* $gdb_dir/* $pkg_dir
 	print_msg "Stripping..."
-	find $TARGET-${NAME[4]} | xargs file | grep -e "executable" -e "shared object" | grep ELF \
+	find $pkg_dir | xargs file | grep -e "executable" -e "shared object" | grep ELF \
 	  | cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null
 	print_msg "Make package"
-	tar cf - $TARGET-${NAME[4]} | bzip2 -f > ../$TARGET-${NAME[4]}.tar.bz2
+	tar cf - $pkg_dir | bzip2 -f > ../${pkg_dir}.tar.bz2
 }
 
 echo "" > $LOG_FILE
@@ -156,6 +283,6 @@ pushd $TMP_DIR > /dev/null
 
 print_msg "directory is $TMP_DIR"
 
-do_download && do_unpack && do_binutils && do_gcc && do_gdb && makepkg
+do_download && do_unpack && do_gmp && do_mpfr && do_mpc && do_binutils && do_gcc && do_gdb && makepkg
 
 popd > /dev/null
