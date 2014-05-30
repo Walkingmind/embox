9d8b59ebfffe11eb9b0f247d1980bd34287357d5
anton.bondarev2310@gmail.com <anton.bondarev2310@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-05-30 13:27:58 +0000

    add POSIX header regex.h

--- trunk/embox/src/include/limits.h
+++ trunk/embox/src/include/limits.h
@@ -114,5 +114,8 @@ Maximum number of bytes in a character, for any supported locale.
     Minimum Acceptable Value: 1
 */
 #define MB_LEN_MAX 1
-    
+
+//TODO life is pain
+#include <ctype.h>
+
 #endif /* LIMITS_H_ */
