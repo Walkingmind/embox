7963c4ed6c7459a457d43332e3da901df468349a
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-06-16 12:53:16 +0000

    Merged revision(s) 13718-14063 from trunk/embox

---
+++ branches/fs-dcache/platform/zrv/cmds/smac_socket/smac_socket_server.c
@@ -0,0 +1,51 @@
+/**
+ * @file
+ *
+ * @date Jun 9, 2014
+ * @author: Anton Bondarev
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#include <embox/cmd.h>
+
+EMBOX_CMD(smac_socket_main);
+
+static void usage(char *cmd) {
+	printf("Usage: %s start as server\n ip_addr:port\n", cmd);
+}
+
+static int smac_socket_start_server(char *argv[]) {
+	int res;
+
+	res = vfork();
+
+	if (-1 == res) {
+		printf("error: couldn't vfork()\n");
+		return -1;
+	}
+	if (0 == res) {
+		execv("smac_socket_daemon", argv);
+	}
+
+	return 0;
+}
+
+static int smac_socket_main(int argc, char *argv[]) {
+	int opt;
+
+	while (-1 != (opt = getopt(argc, argv, "h"))) {
+		switch(opt) {
+		case '?':
+		case 'h': /* show help */
+			usage(argv[0]);
+			return ENOERR;
+		}
+	}
+
+	return smac_socket_start_server(argv);
+}
