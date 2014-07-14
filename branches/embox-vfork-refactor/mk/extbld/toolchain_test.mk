a243cb5120040d181dca3fc1eaf37f2977024c86
deryugin.denis@gmail.com <deryugin.denis@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-07-14 10:53:40 +0000

    Removing [vfork-refactor] due to needlessness

--- branches/embox-vfork-refactor/mk/extbld/toolchain_test.mk
+++
@@ -1,9 +0,0 @@
-
-TOOLCHAIN_TEST_SRC := $(ROOT_DIR)/mk/extbld/toolchain_test.c
-TOOLCHAIN_TEST_OUT := $(OBJ_DIR)/toolchain_test
-
-include $(EXTBLD_LIB)
-
-.PHONY : do_test
-do_test : 
-	EMBOX_GCC_LINK=full $(EMBOX_GCC) $(TOOLCHAIN_TEST_SRC) -o $(TOOLCHAIN_TEST_OUT)
