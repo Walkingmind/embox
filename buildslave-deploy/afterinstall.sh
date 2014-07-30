1d123196a9b8d9c207e3d93d6fe3c209c68f8686
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-07-30 13:05:01 +0000

    [buildslave-deploy] flex and bison required

--- buildslave-deploy/afterinstall.sh
+++ buildslave-deploy/afterinstall.sh
@@ -38,7 +38,7 @@ install_tsim() {
 }
 
 install_bootstrap() {
-	$APT_GET_INSTALL openssh-server vim subversion build-essential libncurses5-dev gcc-multilib g++-multilib texinfo 
+	$APT_GET_INSTALL openssh-server vim subversion build-essential libncurses5-dev gcc-multilib g++-multilib texinfo flex bison
 	$APT_GET_INSTALL cmake u-boot-tools libgmp-dev libmpfr-dev libmpc-dev #build dependencies for embox
 	install_tsim
 	enable_autostart ssh
