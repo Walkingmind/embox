5f57d804868f6c47fc667a4b520e12a79f9968cc
Vita.Loginova@gmail.com <Vita.Loginova@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-08-15 12:41:33 +0000

    removing lthread branch

--- branches/lthread/third-party/lua/include/locale.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUA_LOCALE_H_
-#define LUA_LOCALE_H_
-
-#include_next <locale.h>
-
-#define getlocaledecpoint() '.'
-
-#endif /* LUA_LOCALE_H_ */
--- branches/lthread/third-party/lua/tofi_mod.lua
+++
@@ -1,8 +0,0 @@
-local filename = arg[1]
-io.write("filename: " .. filename .. "\n")
-local ouf = assert(io.open(filename, "w"))
-ouf:write("1\n")
-ouf:write("2\n")
-ouf:write("3\n")
-ouf:close()
-
