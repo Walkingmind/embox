fea3454673caac3d4a4f2725c6dfa400b30e18a0
drakon.mega <drakon.mega@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2014-09-24 08:36:12 +0000

    [embox-mmu] deleted

--- branches/embox-mmu/third-party/lib/luasocket/http_request.lua
+++
@@ -1,8 +0,0 @@
-
-http = require("socket.http")
-
-local Body, Cde, HDR, StartsStr = http.request(arg[1])
-print(Body)
-print(Cde)
-
-os.exit(Body ~= nil)
--- branches/embox-mmu/third-party/lib/luasocket/include/lua.h
+++
@@ -1,8 +0,0 @@
-#ifndef LUASOCKET_LUA_H_
-#define LUASOCKET_LUA_H_
-
-#include_next <lua.h>
-
-#include <linux/types.h>
-
-#endif /* LUASOCKET_LUA_H_ */
