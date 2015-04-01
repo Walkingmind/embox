dae326d146cc124e6bd26cacab09bfc7a9b15a53
deryugin.denis@gmail.com <deryugin.denis@gmail.com@5c469cbc-2eb3-11df-bba7-995ec2794b1c>
2015-04-01 15:49:15 +0000

    [vfs] working on dvfs_lookup

--- src/include/fs/hlpr_path.h
+++ src/include/fs/hlpr_path.h
@@ -17,7 +17,7 @@ extern int path_is_double_dot(const char *path);
  * For example,
  * for '/fooo/bar/' returns 'fooo/bar/' and 4 on plen
  * for '/bar/' ('fooo/bar/' + 4) returns 'bar/', 3
- * for '/' ('bar/' + 3) return NULL 
+ * for '/' ('bar/' + 3) return NULL
  *
  * @param path Path to process
  * @param p_len Pointer to store first node name's length
